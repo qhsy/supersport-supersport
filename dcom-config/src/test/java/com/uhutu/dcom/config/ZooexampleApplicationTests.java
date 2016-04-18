6e906d2560a2426a26f0ec357969bdc32ea169e1
pangjh <pangjha@ichsy.com>
2016-04-18 21:41:49 +0800

    x

---
+++ dcom-config/src/test/java/com/uhutu/dcom/config/ZooexampleApplicationTests.java
@@ -0,0 +1,18 @@
+package com.uhutu.dcom.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.uhutu.dcom.config.ZooexampleApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ZooexampleApplication.class)
+public class ZooexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
