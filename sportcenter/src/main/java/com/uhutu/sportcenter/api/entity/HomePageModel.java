ad145e2c34b7cd38b4b5d5f72071dcde3368ccba
xiegj <xiegj@ichsy.com>
2016-04-19 15:46:20 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/api/entity/HomePageModel.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/api/entity/HomePageModel.java
@@ -13,6 +13,9 @@ public class HomePageModel {
 	@ApiModelProperty(name="展示类型" ,notes=" 01:展示图文，02:轮播图，	03:轮播图",example="01")
 	private String type="";
 	
+	@ApiModelProperty(name="展示顺序" ,notes="0或者1或者2,越小越靠上,最小为0",example="0")
+	private String sort	=	"";
+	
 	@ApiModelProperty(name="头像图片链接" ,notes="头像图片链接",example="http://www.ichsy.com")
 	private String headProUrl="";
 	
@@ -22,8 +25,11 @@ public class HomePageModel {
 	@ApiModelProperty(name="标题" ,notes="标题",example="新体优家，您运动的首选~")
 	private String title="";
 	
-	@ApiModelProperty(name="位置" ,notes="位置经纬度",example="116.404, 39.915")
+	@ApiModelProperty(name="地理位置" ,notes="位置经纬度",example="116.404, 39.915")
 	private String location="";
+	
+	@ApiModelProperty(name="地理位置名称" ,notes="位置名称",example="金域国际大厦")
+	private String locationName="";
 	
 	@ApiModelProperty(name="昵称" ,notes="昵称",example="papi酱")
 	private String nickname="";
@@ -107,6 +113,22 @@ public class HomePageModel {
 
 	public void setDateShow(String dateShow) {
 		this.dateShow = dateShow;
+	}
+
+	public String getSort() {
+		return sort;
+	}
+
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
 	}
 	
 }
