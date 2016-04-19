e9303dfb4a29b35de98b6a08cc8fb5c95f3a205a
pang_jhui <pangjha@ichsy.com>
2016-04-19 14:48:01 +0800

    x

--- sportcloud/src/main/java/com/uhutu/sportcloud/SportCloudApplication.java
+++ sportcloud/src/main/java/com/uhutu/sportcloud/SportCloudApplication.java
@@ -3,8 +3,10 @@ package com.uhutu.sportcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uhutu.zoocom.root.RootSimpleApplication;
+
 @SpringBootApplication
-public class SportCloudApplication {
+public class SportCloudApplication extends RootSimpleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportCloudApplication.class, args);
