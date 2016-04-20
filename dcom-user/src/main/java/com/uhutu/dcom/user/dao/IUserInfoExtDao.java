6502958247a52635b8edfe76bf4a2da93576493b
pang_jhui <pangjha@ichsy.com>
2016-04-20 19:10:30 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/dao/IUserInfoExtDao.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/dao/IUserInfoExtDao.java
@@ -1,7 +1,6 @@
 package com.uhutu.dcom.user.dao;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.uhutu.dcom.user.entity.UcUserinfoExt;
 
@@ -10,7 +9,6 @@ import com.uhutu.dcom.user.entity.UcUserinfoExt;
  * @author pang_jhui
  *
  */
-@Repository
 public interface IUserInfoExtDao extends CrudRepository<UcUserinfoExt, Long> {
 
 }
--- dcom-user/src/main/java/com/uhutu/dcom/user/dao/IUserInfoSocialDao.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/dao/IUserInfoSocialDao.java
@@ -1,7 +1,6 @@
 package com.uhutu.dcom.user.dao;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.uhutu.dcom.user.entity.UcUserinfoSocial;
 
@@ -10,7 +9,7 @@ import com.uhutu.dcom.user.entity.UcUserinfoSocial;
  * @author pang_jhui
  *
  */
-@Repository
+
 public interface IUserInfoSocialDao extends CrudRepository<UcUserinfoSocial, Long> {
 
 }
