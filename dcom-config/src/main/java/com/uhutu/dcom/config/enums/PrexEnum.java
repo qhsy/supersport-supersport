59c78994e7f3bbfd821b3cacc839fdcdcb0de6e4
pang_jhui <pangjha@ichsy.com>
2016-04-20 20:11:23 +0800

    x

---
+++ dcom-config/src/main/java/com/uhutu/dcom/config/enums/PrexEnum.java
@@ -0,0 +1,28 @@
+package com.uhutu.dcom.config.enums;
+
+/**
+ * 编号前缀相关枚举
+ * @author pang_jhui
+ *
+ */
+public enum PrexEnum {
+	
+	/**用户信息编号前缀*/
+	UC,
+	/**内容基本信息前缀*/
+	CNB,
+	/**内容分类前缀*/
+	CNC,
+	/**内容评价前缀*/
+	CNE,
+	/**内容栏目前缀*/
+	CNI,
+	/**内容标签前缀*/
+	CNL,
+	/**内容评论前缀*/
+	CNR,
+	/**运动分类前缀*/
+	SPC;
+	
+
+}
