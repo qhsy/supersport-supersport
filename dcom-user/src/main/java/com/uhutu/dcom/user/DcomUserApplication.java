78eff73f2e0c341956ce5d625161d71363a6739b
srnpr <srnpr@qh-nt>
2016-04-20 22:45:02 +0800

    a

--- dcom-user/src/main/java/com/uhutu/dcom/user/DcomUserApplication.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/DcomUserApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DcomUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DcomUserApplicationTest.class, args);
+		SpringApplication.run(DcomUserApplication.class, args);
 	}
 }
