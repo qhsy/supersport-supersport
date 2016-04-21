25c6228f3020a8bd2c6a71851ab34f7b4f0489a8
pang_jhui <pangjha@ichsy.com>
2016-04-21 17:46:23 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/api/ApiSendSms.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/api/ApiSendSms.java
@@ -1,7 +1,10 @@
 package com.uhutu.sportcenter.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.uhutu.dcom.user.entity.UcUserinfo;
+import com.uhutu.dcom.user.service.UserServiceFactory;
 import com.uhutu.sportcenter.api.input.ApiSendSmsInput;
 import com.uhutu.sportcenter.api.result.ApiSendSmsResult;
 import com.uhutu.zoocom.root.RootApiBase;
@@ -13,10 +16,25 @@ import com.uhutu.zoocom.root.RootApiBase;
  */
 @Service
 public class ApiSendSms extends RootApiBase< ApiSendSmsInput,ApiSendSmsResult> {
+	
+	@Autowired
+	private UserServiceFactory userServiceFactory;
 
 	public ApiSendSmsResult process(ApiSendSmsInput inputParam ) {
 		
-		return new ApiSendSmsResult();
+		ApiSendSmsResult sendSmsResult = new ApiSendSmsResult();
+		
+		UcUserinfo ucUserinfo = userServiceFactory.getUserInfoService().queryByLoginName(inputParam.getMobileNO());
+		
+		if(ucUserinfo != null){
+			
+			sendSmsResult.setError("该用户已经注册");
+			
+			sendSmsResult.setStatus(0);
+			
+		}
+		
+		return sendSmsResult;
 	}
 
 }
