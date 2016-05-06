7de5fc067a4849a7dfe1a166fa08b6379d5da58b
pang_jhui <pangjha@ichsy.com>
2016-05-06 16:34:44 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/ApiPublishContentPhotos.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/ApiPublishContentPhotos.java
@@ -1,5 +1,8 @@
 package com.uhutu.sportcenter.z;
 
+import org.springframework.beans.BeanUtils;
+
+import com.uhutu.dcom.content.z.entity.CnContentBasicinfo;
 import com.uhutu.sportcenter.z.input.ApiPublishContentPhotosInput;
 import com.uhutu.sportcenter.z.result.ApiPublishContentPhotosResult;
 import com.uhutu.zoocom.root.RootApiToken;
@@ -18,6 +21,12 @@ public class ApiPublishContentPhotos extends RootApiToken<ApiPublishContentPhoto
 		ApiPublishContentPhotosResult contentPhotosResult = new ApiPublishContentPhotosResult();
 
 		if (input.getContentBasicInfo() != null && input.getContentPhotos() != null) {
+			
+			CnContentBasicinfo contentBasicinfo = new CnContentBasicinfo();
+			
+			BeanUtils.copyProperties(input.getContentBasicInfo(), contentBasicinfo);
+			
+			contentBasicinfo.setAuthor(input.getZoo().getToken());
 
 		} else {
 
