70d70689c5539adc55523c3c87399444d32485dc
srnpr <srnpr@qh-nt>
2016-05-06 14:22:51 +0800

    a

---
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/control/HomeController.java
@@ -0,0 +1,15 @@
+package com.uhutu.sportcenter.z.control;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 体育相关统一接入
+ * @author pang_jhui
+ *
+ */
+@RestController
+public class HomeController {
+	
+	
+
+}
