899734108c3a496fcf5071118395bfbc8f0c5f75
xiegj <xiegj@ichsy.com>
2016-05-09 11:44:35 +0800

    x

---
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentRemarkPage.java
@@ -0,0 +1,11 @@
+package com.uhutu.dcom.content.z.page;
+
+import com.uhutu.dcom.content.z.entity.CnContentRemark;
+import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.root.RootPageSimple;
+import com.uhutu.zooweb.define.DefineZooWeb;
+
+@ZooPage(name = "标签页面", pages = { DefineZooWeb.PAGE_DEFAULT_SUB})
+public class ContentRemarkPage extends RootPageSimple<CnContentRemark> {
+
+}
