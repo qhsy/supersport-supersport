b0cb07a688ed6a4d49c3f82473b53470edde1686
逄小帅 <@DESKTOP-P9ASMJO>
2016-05-16 16:27:39 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnContentRecomm.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnContentRecomm.java
@@ -16,7 +16,7 @@ public class CnContentRecomm extends BaseEntity {
 	@ZooData(name="内容编号")
 	private String contentCode;
 	
-	@ZooData(name="内容信息")
+	@ZooData(name="内容信息",sort={"pq=0"})
 	private String content;
 
 	public String getContentCode() {
---
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentRecommPage.java
@@ -0,0 +1,15 @@
+package com.uhutu.dcom.content.z.page;
+
+import com.uhutu.dcom.content.z.entity.CnContentRecomm;
+import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.root.RootPageSimple;
+
+/**
+ * 小编推荐相关内容页面
+ * @author 逄小帅
+ *
+ */
+@ZooPage(name="小编推荐页面")
+public class ContentRecommPage extends RootPageSimple<CnContentRecomm> {
+
+}
