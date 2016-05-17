981290e01e119597ae859313fa70f8741ec3ff3c
xiegj <xiegj@ichsy.com>
2016-05-17 11:31:18 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnAdvertiseInfo.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnAdvertiseInfo.java
@@ -3,6 +3,9 @@ package com.uhutu.dcom.content.z.entity;
 import javax.persistence.Entity;
 
 import com.uhutu.zoocom.baseannotation.ZooData;
+import com.uhutu.zoocom.define.DefineWebElement;
+import com.uhutu.zoocom.define.DefineWebInc;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoodata.dbbase.BaseEntity;
 
 /**
@@ -15,13 +18,16 @@ import com.uhutu.zoodata.dbbase.BaseEntity;
 @Entity
 public class CnAdvertiseInfo extends BaseEntity {
 
-	@ZooData(name = "广告编号")
+	@ZooData(name = "广告编号", inc = DefineWebInc.Insert_Code + "=ADBH", sort = { DefineWebPage.Page_Add + "=35",
+			DefineWebPage.Page_Edit + "=0" })
 	private String code;
 
-	@ZooData(name = "广告类型")
+	@ZooData(name = "广告类型", element = DefineWebElement.Select, inc = {
+			DefineWebInc.System_Define + "=dzsd469910011004" })
 	private String type;
 
-	@ZooData(name = "广告状态")
+	@ZooData(name = "广告状态(是否发布)", element = DefineWebElement.Select, inc = {
+			DefineWebInc.System_Define + "=dzsd469910011001" })
 	private String status;
 
 	public String getCode() {
