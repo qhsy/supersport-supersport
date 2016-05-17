1514b6030a1b73a2343272fb091c87e3cf1abe96
xiegj <xiegj@ichsy.com>
2016-05-17 10:52:53 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnContentItem.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnContentItem.java
@@ -3,6 +3,8 @@ package com.uhutu.dcom.content.z.entity;
 import javax.persistence.Entity;
 
 import com.uhutu.zoocom.baseannotation.ZooData;
+import com.uhutu.zoocom.define.DefineWebInc;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoodata.dbbase.BaseEntity;
 
 /**
@@ -12,14 +14,14 @@ import com.uhutu.zoodata.dbbase.BaseEntity;
  *
  */
 @Entity
-public class CnContentItem extends BaseEntity{
+public class CnContentItem extends BaseEntity {
 
-	@ZooData(name = "栏目编号")
+	@ZooData(name = "栏目编号", inc = DefineWebInc.Insert_Code + "=LMBH", sort = { DefineWebPage.Page_Add + "=35",
+			DefineWebPage.Page_Edit + "=0" })
 	private String code;
-	
+
 	@ZooData(name = "栏目名称")
 	private String name;
-
 
 	public String getCode() {
 		return code;
--- dcom-tag/src/main/java/com/uhutu/dcom/tag/z/entity/CnContentLabel.java
+++ dcom-tag/src/main/java/com/uhutu/dcom/tag/z/entity/CnContentLabel.java
@@ -3,6 +3,9 @@ package com.uhutu.dcom.tag.z.entity;
 import javax.persistence.Entity;
 
 import com.uhutu.zoocom.baseannotation.ZooData;
+import com.uhutu.zoocom.define.DefineWebElement;
+import com.uhutu.zoocom.define.DefineWebInc;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoodata.dbbase.BaseEntity;
 
 import io.swagger.annotations.ApiModel;
@@ -11,22 +14,24 @@ import io.swagger.annotations.ApiModel;
 @ApiModel
 public class CnContentLabel extends BaseEntity {
 
-	@ZooData(name = "标签编号")
+	@ZooData(name = "标签编号", inc = DefineWebInc.Insert_Code + "=GGBH", sort = { DefineWebPage.Page_Add + "=35",
+			DefineWebPage.Page_Edit + "=0" })
 	private String code;
 
 	@ZooData(name = "标签名称")
 	private String name;
 
-	@ZooData(name = "账户类型")
+	@ZooData(name = "账户类型", sort = { DefineWebPage.Page_Query + "=0" })
 	private String type;
 
-	@ZooData(name = "位置经纬度")
+	@ZooData(name = "位置经纬度", sort = { DefineWebPage.Page_Query + "=0" })
 	private String location;
 
 	@ZooData(name = "位置名称")
 	private String locationName;
 
-	@ZooData(name = "状态")
+	@ZooData(name = "状态(是否可用)", element = DefineWebElement.Select, inc = {
+			DefineWebInc.System_Define + "=dzsd469910011001" })
 	private String status;
 
 	public String getCode() {
