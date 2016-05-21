281cbb4c0a33e1a0ebc882b4cbae7ef88ad13ac4
xiegj <xiegj@ichsy.com>
2016-05-21 17:36:37 +0800

    xx

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiHomePageResult.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiHomePageResult.java
@@ -3,37 +3,25 @@ package com.uhutu.sportcenter.z.result;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.uhutu.sportcenter.z.entity.AdvertiseMent;
 import com.uhutu.sportcenter.z.entity.HomePageModel;
 import com.uhutu.zoocom.root.RootApiResult;
 
 import io.swagger.annotations.ApiModelProperty;
 
 public class ApiHomePageResult extends RootApiResult {
-	
-	@ApiModelProperty(name="首页展示实体数组" ,notes="首页展示实体数组",example="01")
-	private List<HomePageModel>	list=	new  ArrayList<HomePageModel>();
 
-	@ApiModelProperty(name="首页顶部广告位" ,notes="首页顶部广告位")
-	private AdvertiseMent	adv = new AdvertiseMent();
-	
-	@ApiModelProperty(value = "是否还有下一页", notes = "是否还有下一页 ", example = "true", required = true)
+	@ApiModelProperty(name = "首页展示实体数组", notes = "首页展示实体数组", example = "01")
+	private List<HomePageModel> list = new ArrayList<HomePageModel>();
+
+	@ApiModelProperty(value = "是否还有前一天数据", notes = "是否还有前一天数据", example = "true", required = true)
 	private boolean nextflag = true;
-	
+
 	public List<HomePageModel> getList() {
 		return list;
 	}
 
 	public void setList(List<HomePageModel> list) {
 		this.list = list;
-	}
-
-	public AdvertiseMent getAdv() {
-		return adv;
-	}
-
-	public void setAdv(AdvertiseMent adv) {
-		this.adv = adv;
 	}
 
 	public boolean isNextflag() {
