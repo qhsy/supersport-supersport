d81483b3776ed17303bf26725084ef6e1c0316c0
xiegj <xiegj@ichsy.com>
2016-05-23 11:18:48 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnContentRecomm.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnContentRecomm.java
@@ -4,7 +4,9 @@ import javax.persistence.Entity;
 
 import com.uhutu.zoocom.baseannotation.ZooData;
 import com.uhutu.zoocom.define.DefineWebElement;
+import com.uhutu.zoocom.define.DefineWebInc;
 import com.uhutu.zoocom.define.DefineWebPage;
+import com.uhutu.zoocom.define.DefineWebVerify;
 import com.uhutu.zoodata.dbbase.BaseEntity;
 
 /**
@@ -16,11 +18,18 @@ import com.uhutu.zoodata.dbbase.BaseEntity;
 @Entity
 public class CnContentRecomm extends BaseEntity {
 
-	@ZooData(name = "内容编号")
+	@ZooData(name = "内容", element = DefineWebElement.Model, inc = { DefineWebInc.Web_Component + "=dzcw410710010002" })
 	private String contentCode;
 
 	@ZooData(name = "推荐理由", sort = { DefineWebPage.Page_Query + "=0" }, element = DefineWebElement.Textarea)
 	private String content;
+
+	@ZooData(name = "推荐标题", verify = { DefineWebVerify.Max_Length + "=28" })
+	private String title;
+
+	@ZooData(name = "推荐封面", element = DefineWebElement.Upload, sort = { DefineWebPage.Page_Query + "=0",
+			DefineWebPage.Page_Grid + "=0" })
+	private String cover;
 
 	public String getContentCode() {
 		return contentCode;
@@ -36,6 +45,22 @@ public class CnContentRecomm extends BaseEntity {
 
 	public void setContent(String content) {
 		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCover() {
+		return cover;
+	}
+
+	public void setCover(String cover) {
+		this.cover = cover;
 	}
 
 }
