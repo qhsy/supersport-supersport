bba6fe019e2b927280eb7f4c7c44a92ae08c2d4a
逄小帅 <@DESKTOP-P9ASMJO>
2016-05-24 20:49:42 +0800

    x

---
+++ dcom-component/src/main/java/com/uhutu/dcom/component/z/util/FileUtil.java
@@ -0,0 +1,45 @@
+package com.uhutu.dcom.component.z.util;
+
+import java.io.ByteArrayOutputStream;
+import org.apache.http.HttpEntity;
+import com.uhutu.zoocom.support.WebClientSupport;
+import com.uhutu.zooweb.support.WebUploadSupport;
+
+/**
+ * 文件工具类
+ * @author 逄小帅
+ *
+ */
+public class FileUtil {
+	
+	/**
+	 * 阿里云转本地文件服务器
+	 * @param fileName
+	 * 		自定义文件名称（须加扩展名）
+	 * @param url
+	 * 		阿里云图像url
+	 * @throws Exception
+	 */
+	public void aliyunConvert(String fileName,String url) throws Exception{
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+		HttpEntity httpEntity = WebClientSupport.create().upEntity(url);
+
+		byte[] bytes = new byte[1024];
+
+		int count = 0;
+
+		while ((count = httpEntity.getContent().read(bytes)) != -1) {
+
+			bos.write(bytes, 0, count);
+
+		}
+		
+		WebUploadSupport support = new WebUploadSupport();
+		
+		support.remoteUpload("zoofile", fileName, bos.toByteArray());
+		
+	}
+
+}
