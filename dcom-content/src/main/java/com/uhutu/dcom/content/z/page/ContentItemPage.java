b10a9198fb895cde76e1dafaa87bc83bf81238dd
xiegj <xiegj@ichsy.com>
2016-05-25 16:08:11 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentItemPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentItemPage.java
@@ -2,10 +2,12 @@ package com.uhutu.dcom.content.z.page;
 
 import com.uhutu.dcom.content.z.entity.CnContentItem;
 import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
-@ZooPage(name = "栏目管理", pages = { DefineZooWeb.PAGE_DEFAULT_SUB})
+@ZooPage(name = "栏目管理", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
+		DefineWebPage.Page_Add + "=dzoi41071007", DefineWebPage.Page_Edit + "=dzoi41071008" })
 public class ContentItemPage extends RootPageSimple<CnContentItem> {
 
 }
