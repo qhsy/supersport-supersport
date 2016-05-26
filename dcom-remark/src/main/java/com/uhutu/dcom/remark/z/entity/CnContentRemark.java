bcb693f3f0029616ff2fdbd8f5fae976465b5d4d
xiegj <xiegj@ichsy.com>
2016-05-26 14:28:39 +0800

    x

--- dcom-remark/src/main/java/com/uhutu/dcom/remark/z/entity/CnContentRemark.java
+++ dcom-remark/src/main/java/com/uhutu/dcom/remark/z/entity/CnContentRemark.java
@@ -3,32 +3,38 @@ package com.uhutu.dcom.remark.z.entity;
 import javax.persistence.Entity;
 
 import com.uhutu.zoocom.baseannotation.ZooData;
+import com.uhutu.zoocom.define.DefineWebElement;
+import com.uhutu.zoocom.define.DefineWebInc;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoodata.dbbase.BaseEntity;
 
 /**
  * 内容评论信息
+ * 
  * @author 逄小帅
  *
  */
 @Entity
 public class CnContentRemark extends BaseEntity {
-	
-	@ZooData(name="评论编号")
+
+	@ZooData(name = "评论编号", sort = { DefineWebPage.Page_Edit + "=0" })
 	private String code;
-	
-	@ZooData(name="内容编号")
+
+	@ZooData(name = "内容标题", element = DefineWebElement.Model, inc = {
+			DefineWebInc.Web_Component + "=dzcw410710010002" }, sort = { DefineWebPage.Page_Edit + "=0" })
 	private String contentCode;
-	
-	@ZooData(name="评论内容")
+
+	@ZooData(name = "评论内容", sort = { DefineWebPage.Page_Edit + "=0" })
 	private String remark;
-	
-	@ZooData(name="作者编号")
+
+	@ZooData(name = "作者名称", element = DefineWebElement.Model, inc = {
+			DefineWebInc.Web_Component + "=dzcw451010010001" }, sort = { DefineWebPage.Page_Edit + "=0" })
 	private String author;
-	
-	@ZooData(name="父级编号")
+
+	@ZooData(name = "父级编号", sort = { DefineWebPage.Page_Edit + "=0" })
 	private String parentCode;
-	
-	@ZooData(name="评论状态")
+
+	@ZooData(name = "是否通过", element = DefineWebElement.Select, inc = { DefineWebInc.System_Define + "=10" })
 	private String status;
 
 	public String getCode() {
@@ -78,7 +84,5 @@ public class CnContentRemark extends BaseEntity {
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
-	
 
 }
--- dcom-remark/src/main/java/com/uhutu/dcom/remark/z/page/ContentRemarkPage.java
+++ dcom-remark/src/main/java/com/uhutu/dcom/remark/z/page/ContentRemarkPage.java
@@ -5,7 +5,8 @@ import com.uhutu.zoocom.baseannotation.ZooPage;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
-@ZooPage(name = "评论信息", pages = { DefineZooWeb.PAGE_DEFAULT_SUB })
+@ZooPage(name = "评论信息", pages = {
+		DefineZooWeb.PAGE_DEFAULT_SUB }, operates = "pe=dzoi469910011005&pg=dzoi469910011009&pq=dzoi469910021017")
 public class ContentRemarkPage extends RootPageSimple<CnContentRemark> {
 
 }
