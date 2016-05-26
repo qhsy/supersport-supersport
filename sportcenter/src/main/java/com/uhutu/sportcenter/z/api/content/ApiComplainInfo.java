cdad2f606e3357030682f9cd2d8f2d8cc80032d7
逄小帅 <@DESKTOP-P9ASMJO>
2016-05-26 11:23:18 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiComplainInfo.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiComplainInfo.java
@@ -1,10 +1,14 @@
 package com.uhutu.sportcenter.z.api.content;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import com.uhutu.dcom.config.enums.PrexEnum;
+import com.uhutu.dcom.remark.z.entity.CnSupportComplain;
+import com.uhutu.dcom.remark.z.service.ContentRemarkServiceFactory;
 import com.uhutu.sportcenter.z.input.ApiComplainInfoInput;
 import com.uhutu.sportcenter.z.result.ApiComplainInfoResult;
 import com.uhutu.zoocom.root.RootApiBase;
+import com.uhutu.zooweb.helper.WebHelper;
 
 /**
  * 投诉接口业务处理
@@ -13,11 +17,30 @@ import com.uhutu.zoocom.root.RootApiBase;
  */
 @Component
 public class ApiComplainInfo extends RootApiBase<ApiComplainInfoInput, ApiComplainInfoResult> {
+	
+	@Autowired
+	private ContentRemarkServiceFactory serviceFacotry;
 
 	@Override
 	protected ApiComplainInfoResult process(ApiComplainInfoInput input) {
 		
-		return new ApiComplainInfoResult();
+		ApiComplainInfoResult result = new ApiComplainInfoResult();
+		
+		CnSupportComplain supportComplain = new CnSupportComplain();
+		
+		supportComplain.setCode(WebHelper.upCode(PrexEnum.CCP.name()));
+		
+		supportComplain.setContentCode(input.getContentCode());
+		
+		supportComplain.setReasonCode(input.getReasonCode());
+		
+		supportComplain.setRemark(input.getRemark());
+		
+		supportComplain.setType(input.getType());
+		
+		serviceFacotry.getSupportComplainService().save(supportComplain);
+		
+		return result;
 		
 	}
 
