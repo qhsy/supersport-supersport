7374abe52bc90036cb729a7d768c4b8456759c2d
逄小帅 <@DESKTOP-P9ASMJO>
2016-06-01 18:02:51 +0800

    马甲用户注册

---
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/page/func/VestUserRegFunc.java
@@ -0,0 +1,62 @@
+package com.uhutu.dcom.user.z.page.func;
+
+import com.uhutu.zoocom.define.DefineUser;
+import com.uhutu.zoodata.z.helper.JdbcHelper;
+import com.uhutu.zooweb.api.webpage.WebOperateInput;
+import com.uhutu.zooweb.api.webpage.WebOperateResult;
+import com.uhutu.zooweb.api.webpage.WebPageModel;
+import com.uhutu.zooweb.model.ExtendPageDefine;
+import com.uhutu.zooweb.root.RootFunc;
+import com.uhutu.zooweb.user.UserCallFactory;
+import com.uhutu.zooweb.user.UserReginsterInput;
+import com.uhutu.zooweb.user.UserReginsterResult;
+
+/**
+ * 马甲用户注册
+ * @author 逄小帅
+ *
+ */
+public class VestUserRegFunc extends RootFunc {
+
+	@Override
+	public WebOperateResult process(WebPageModel webPageModel, ExtendPageDefine extendPageDefine,
+			WebOperateInput input) {
+		
+		WebOperateResult operResult = new WebOperateResult();
+		
+		String loginName = input.getDataMap().get("login_name");
+		
+		String loginPwd = input.getDataMap().get("login_pwd");
+		
+		UserCallFactory userCallFactory = new UserCallFactory();
+		
+		UserReginsterInput registInput = new UserReginsterInput();
+		
+		registInput.setLoginName(loginName);
+		
+		registInput.setLoginPass(loginPwd);
+		
+		registInput.setLoginSystem(DefineUser.Login_System_Default);
+		
+		UserReginsterResult registResult = userCallFactory.userReginster(registInput);
+		
+		if(registResult.upFlagTrue()){
+			
+			input.getDataMap().put("login_code", registResult.getLoginCode());
+			
+			input.getDataMap().put("code", registResult.getUserCode());
+			
+			JdbcHelper.dataInsert(extendPageDefine.getPageSource().getTableName(), input.getDataMap());
+			
+		}else{
+			
+			operResult.setError(registResult.getError());
+			
+			operResult.setStatus(registResult.getStatus());
+			
+		}
+		
+		return operResult;
+	}
+
+}
