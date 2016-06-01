88d439260e836588b9a3afa1b664550865269433
xiegj <xiegj@ichsy.com>
2016-06-01 21:37:47 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentBasicInfoForPhotoPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentBasicInfoForPhotoPage.java
@@ -9,7 +9,8 @@ import com.uhutu.zooweb.define.DefineZooWeb;
 
 @ZooPage(name = "图集管理", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = {
 		DefineWebPage.Page_Grid + "=dzoi469910011009,dzoi41071023,dzoi41071024",
-		DefineWebPage.Page_Query + "=dzoi469910011017" }, deploy = {
+		DefineWebPage.Page_Query + "=dzoi469910011017", DefineWebPage.Page_Add + "=dzoi4699100110010001",
+		DefineWebPage.Page_Edit + "=dzoi4699100110051001" }, deploy = {
 				DefineWebDeploy.Grid_Where + "=status!='del' and content_type='dzsd4107100110030003' ",
 				DefineWebDeploy.Url_Query + "=contentCode" })
 public class ContentBasicInfoForPhotoPage extends RootPageSimple<CnContentBasicinfo> {
