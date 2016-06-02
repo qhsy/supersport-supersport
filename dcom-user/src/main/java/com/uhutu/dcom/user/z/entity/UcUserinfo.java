a1cf18cf818d311ae50fefc377c95fae14c706ac
xiegj <xiegj@ichsy.com>
2016-06-02 09:49:27 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/entity/UcUserinfo.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/entity/UcUserinfo.java
@@ -5,39 +5,47 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.uhutu.zoocom.baseannotation.ZooData;
+import com.uhutu.zoocom.define.DefineWebElement;
+import com.uhutu.zoocom.define.DefineWebInc;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoodata.dbbase.BaseEntity;
 
 /**
  * 用户信息
+ * 
  * @author pang_jhui
  *
  */
 @Entity
 public class UcUserinfo extends BaseEntity {
-	
-	@ZooData(name="用户编号")
+
+	@ZooData(name = "用户编号")
 	private String code;
-	
-	@ZooData(name="用户类型")
+
+	@ZooData(name = "用户类型", element = DefineWebElement.Select, inc = {
+			DefineWebInc.System_Define + "=dzsd410710031001" }, sort = { DefineWebPage.Page_Query + "=0",
+					DefineWebPage.Page_Edit + "=0" })
 	private String type;
-	
-	@ZooData(name="登录名称")
+
+	@ZooData(name = "登录名称")
 	private String loginName;
-	
-	@ZooData(name="登录密码")
+
+	@ZooData(name = "登录密码", sort = { DefineWebPage.Page_Query + "=0", DefineWebPage.Page_Grid + "=0" })
 	private String loginPwd;
-	
-	@ZooData(name="是否可用")
+
+	@ZooData(name = "状态", element = DefineWebElement.Select, inc = {
+			DefineWebInc.System_Define + "=dzsd410710041001" })
 	private String flag;
-	
-	@ZooData(name="最近登录时间")
+
+	@ZooData(name = "最近登录时间", sort = { DefineWebPage.Page_Query + "=0" , DefineWebPage.Page_Grid + "=0"})
 	private Date lastTime;
-	
-	@ZooData(name="登录编号")
+
+	@ZooData(name = "登录编号", sort = { DefineWebPage.Page_Query + "=0" , DefineWebPage.Page_Grid + "=0"})
 	private String loginCode;
 
 	/**
 	 * 获取用户编号
+	 * 
 	 * @return
 	 */
 	public String getCode() {
@@ -46,6 +54,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 设置用户编号
+	 * 
 	 * @param code
 	 */
 	public void setCode(String code) {
@@ -54,6 +63,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 获取登录名称
+	 * 
 	 * @return
 	 */
 	public String getLoginName() {
@@ -62,6 +72,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 设置登录名称
+	 * 
 	 * @param loginName
 	 */
 	public void setLoginName(String loginName) {
@@ -70,6 +81,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 设置登录密码
+	 * 
 	 * @return
 	 */
 	public String getLoginPwd() {
@@ -78,6 +90,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 设置登录密码
+	 * 
 	 * @param loginPwd
 	 */
 	public void setLoginPwd(String loginPwd) {
@@ -86,6 +99,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 获取是否可用
+	 * 
 	 * @return
 	 */
 	public String getFlag() {
@@ -94,6 +108,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 设置是否可用
+	 * 
 	 * @param flag
 	 */
 	public void setFlag(String flag) {
@@ -110,6 +125,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 获取用户类型
+	 * 
 	 * @return
 	 */
 	public String getType() {
@@ -118,6 +134,7 @@ public class UcUserinfo extends BaseEntity {
 
 	/**
 	 * 设置用户类型
+	 * 
 	 * @param type
 	 */
 	public void setType(String type) {
@@ -131,7 +148,5 @@ public class UcUserinfo extends BaseEntity {
 	public void setLoginCode(String loginCode) {
 		this.loginCode = loginCode;
 	}
-	
-	
 
 }
