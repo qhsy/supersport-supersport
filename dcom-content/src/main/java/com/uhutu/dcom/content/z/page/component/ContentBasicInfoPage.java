0cf79983d27bfcbfe5edd8f25efe436034246679
xiegj <xiegj@ichsy.com>
2016-06-02 17:53:10 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/component/ContentBasicInfoPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/component/ContentBasicInfoPage.java
@@ -2,10 +2,12 @@ package com.uhutu.dcom.content.z.page.component;
 
 import com.uhutu.dcom.content.z.page.vo.CnContentBasicinfo;
 import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.define.DefineWebDeploy;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
-@ZooPage(name = "内容管理", operates = DefineZooWeb.PAGE_DIALOG_OPERATE)
+@ZooPage(name = "内容管理", operates = DefineZooWeb.PAGE_DIALOG_OPERATE, deploy = {
+		DefineWebDeploy.Grid_Where + "=status!='del' " })
 public class ContentBasicInfoPage extends RootPageSimple<CnContentBasicinfo> {
 
 }
