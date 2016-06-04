fc255bbcdcdd2d1e891eb349ca34597ae3d3f20b
xiegj <xiegj@ichsy.com>
2016-06-04 23:54:59 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UcUserAlbumPage.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UcUserAlbumPage.java
@@ -15,8 +15,7 @@ import com.uhutu.zooweb.define.DefineZooWeb;
  */
 @ZooPage(name = "达人图集", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
 		DefineWebPage.Page_Query + "=dzoi4699100110171001",
-		DefineWebPage.Page_Grid + "=dzoi4699100110091001,dzoi41101004,dzoi41101005" }, deploy = {
-				DefineWebDeploy.Url_Query + "=userCode" })
+		DefineWebPage.Page_Grid + "=dzoi4699100110091001" }, deploy = { DefineWebDeploy.Url_Query + "=userCode" })
 public class UcUserAlbumPage extends RootPageSimple<UcUserAlbum> {
 
 }
--- dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UcUserinfoExpertPage.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UcUserinfoExpertPage.java
@@ -2,6 +2,7 @@ package com.uhutu.dcom.user.z.page;
 
 import com.uhutu.dcom.user.z.entity.UcUserinfoExpert;
 import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
@@ -11,7 +12,8 @@ import com.uhutu.zooweb.define.DefineZooWeb;
  * @author xiegj
  *
  */
-@ZooPage(name = "达人信息", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE })
+@ZooPage(name = "达人信息", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
+		DefineWebPage.Page_Grid + "=dzoi4699100110091001,dzoi41101004,dzoi41101005" })
 public class UcUserinfoExpertPage extends RootPageSimple<UcUserinfoExpert> {
 
 }
