2c51c04da4365142780910d14006d0f15e7306f0
xiegj <xiegj@ichsy.com>
2016-06-05 00:03:49 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UcUserinfoExpertPage.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UcUserinfoExpertPage.java
@@ -2,6 +2,7 @@ package com.uhutu.dcom.user.z.page;
 
 import com.uhutu.dcom.user.z.entity.UcUserinfoExpert;
 import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.define.DefineWebDeploy;
 import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
@@ -13,7 +14,8 @@ import com.uhutu.zooweb.define.DefineZooWeb;
  *
  */
 @ZooPage(name = "达人信息", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
-		DefineWebPage.Page_Grid + "=dzoi4699100110091001,dzoi41101004,dzoi41101005" })
+		DefineWebPage.Page_Grid + "=dzoi4699100110091001,dzoi41101004,dzoi41101005" }, deploy = {
+				DefineWebDeploy.Url_Query + "=userCode" })
 public class UcUserinfoExpertPage extends RootPageSimple<UcUserinfoExpert> {
 
 }
