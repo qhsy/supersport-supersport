0ccf5f6e720dceb76102c5d21a5a805dfaef8a1e
逄小帅 <@DESKTOP-P9ASMJO>
2016-06-05 18:44:44 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/donate/ApiUserExpertDetail.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/donate/ApiUserExpertDetail.java
@@ -1,12 +1,7 @@
 package com.uhutu.sportcenter.z.api.donate;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
-import com.uhutu.dcom.component.z.page.QueryConditions;
-import com.uhutu.dcom.user.z.entity.UcUserinfoExpert;
-import com.uhutu.dcom.user.z.service.UserServiceFactory;
 import com.uhutu.sportcenter.z.input.ApiUserExpertDetailInput;
 import com.uhutu.sportcenter.z.result.ApiUserExpertDetailResult;
 import com.uhutu.zoocom.root.RootApiBase;
@@ -19,15 +14,13 @@ import com.uhutu.zoocom.root.RootApiBase;
 @Component
 public class ApiUserExpertDetail extends RootApiBase<ApiUserExpertDetailInput, ApiUserExpertDetailResult> {
 	
-	@Autowired
-	private UserServiceFactory serviceFactory;
 
 	@Override
 	protected ApiUserExpertDetailResult process(ApiUserExpertDetailInput input) {
 		
 		ApiUserExpertDetailResult result = new ApiUserExpertDetailResult();
 		
-		Page<UcUserinfoExpert> expertPage = serviceFactory.getUserInfoExpertService().queryPageByConditon(1, 10, new QueryConditions());
+		
 		
 		
 		
