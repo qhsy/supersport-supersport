646694c01a83ea22c5c7992ea5e651e9b8d948b6
xiegj <xiegj@ichsy.com>
2016-06-05 12:03:10 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/SportCategoryPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/SportCategoryPage.java
@@ -2,10 +2,12 @@ package com.uhutu.dcom.content.z.page;
 
 import com.uhutu.dcom.content.z.entity.SpSportCategory;
 import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.define.DefineWebDeploy;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
-@ZooPage(name = "运动管理", pages = { DefineZooWeb.PAGE_DEFAULT_SUB })
+@ZooPage(name = "运动管理", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, deploy = {
+		DefineWebDeploy.Grid_Order + "= sort desc" })
 public class SportCategoryPage extends RootPageSimple<SpSportCategory> {
 
 }
