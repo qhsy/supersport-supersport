f68e32381e88a90c6b181a2476aae54d892d9ba7
逄小帅 <@DESKTOP-P9ASMJO>
2016-06-06 13:32:18 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/dao/IUserDonateInfoDao.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/dao/IUserDonateInfoDao.java
@@ -14,7 +14,7 @@ import com.uhutu.dcom.user.z.entity.UcDonateInfo;
  */
 public interface IUserDonateInfoDao extends JpaRepository<UcDonateInfo, String>,JpaSpecificationExecutor<UcDonateInfo> {
 	
-	@Query("select t from UcDonateInfo t where supportCode=:supportCode and beSupportCode =: beSupportCode ")
+	@Query("select t from UcDonateInfo t where supportCode=:supportCode and beSupportCode=:beSupportCode ")
 	public UcDonateInfo queryByCode(@Param("supportCode") String supportCode,@Param("beSupportCode") String beSupportCode);
 
 }
