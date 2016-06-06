b947f5c66ca65bd014bec3a9a92877efb04e4879
xiegj <xiegj@ichsy.com>
2016-06-06 10:13:10 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentItemForAdvPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentItemForAdvPage.java
@@ -10,7 +10,8 @@ import com.uhutu.zooweb.define.DefineZooWeb;
 @ZooPage(name = "栏目", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
 		DefineWebPage.Page_Add + "=dzoi41071007", DefineWebPage.Page_Edit + "=dzoi41071008",
 		DefineWebPage.Page_Grid + "=dzoi469910011009,dzoi41071017,dzoi41071018" }, deploy = {
-				DefineWebDeploy.Grid_Order + "=-start_time", DefineWebDeploy.Url_Query + "=code",
+				DefineWebDeploy.Grid_Order + "= DATE_FORMAT(start_time,'%Y-%m-%d') desc,sort desc,zc desc",
+				DefineWebDeploy.Url_Query + "=code",
 				DefineWebDeploy.Grid_Where + "=type in ('dzsd4107100110060001','dzsd4107100110060002') " })
 public class ContentItemForAdvPage extends RootPageSimple<CnContentItem> {
 
--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentItemPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/ContentItemPage.java
@@ -10,8 +10,8 @@ import com.uhutu.zooweb.define.DefineZooWeb;
 @ZooPage(name = "栏目", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
 		DefineWebPage.Page_Add + "=dzoi41071007", DefineWebPage.Page_Edit + "=dzoi41071008",
 		DefineWebPage.Page_Grid + "=dzoi469910011009,dzoi41071015,dzoi41071016" }, deploy = {
-				DefineWebDeploy.Grid_Order + "=-start_time", DefineWebDeploy.Url_Query + "=code",
-				DefineWebDeploy.Grid_Where + "=type='dzsd4107100110060003' " })
+				DefineWebDeploy.Grid_Order + "= DATE_FORMAT(start_time,'%Y-%m-%d') desc,sort desc,zc desc",
+				DefineWebDeploy.Url_Query + "=code", DefineWebDeploy.Grid_Where + "=type='dzsd4107100110060003' " })
 public class ContentItemPage extends RootPageSimple<CnContentItem> {
 
 }
