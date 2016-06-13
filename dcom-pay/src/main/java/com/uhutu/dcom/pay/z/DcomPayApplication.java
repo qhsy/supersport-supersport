b6b7023781e909ee4759d0aa66bcb3b2a40c0cab
逄小帅 <@DESKTOP-P9ASMJO>
2016-06-13 16:25:42 +0800

    x

---
+++ dcom-pay/src/main/java/com/uhutu/dcom/pay/z/DcomPayApplication.java
@@ -0,0 +1,12 @@
+package com.uhutu.dcom.pay.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcomPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcomPayApplication.class, args);
+	}
+}
