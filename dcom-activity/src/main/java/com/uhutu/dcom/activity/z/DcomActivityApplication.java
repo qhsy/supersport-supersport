fc1382793acc9829f4f3015ed41acd642c99bbd9
逄小帅 <@DESKTOP-P9ASMJO>
2016-06-14 17:42:35 +0800

    x

---
+++ dcom-activity/src/main/java/com/uhutu/dcom/activity/z/DcomActivityApplication.java
@@ -0,0 +1,12 @@
+package com.uhutu.dcom.activity.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcomActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcomActivityApplication.class, args);
+	}
+}
