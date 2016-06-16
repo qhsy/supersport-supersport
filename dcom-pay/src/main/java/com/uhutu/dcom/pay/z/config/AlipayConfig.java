8f9c0aa3769b792376160b212f522a1807279d8e
逄小帅 <@DESKTOP-P9ASMJO>
2016-06-16 19:24:45 +0800

    x

--- dcom-pay/src/main/java/com/uhutu/dcom/pay/z/config/AlipayConfig.java
+++ dcom-pay/src/main/java/com/uhutu/dcom/pay/z/config/AlipayConfig.java
@@ -3,13 +3,15 @@ package com.uhutu.dcom.pay.z.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.uhutu.dcom.pay.z.face.IPayConfig;
+
 /**
  * 支付宝配置信息
  * @author pang_jhui
  *
  */
 @Component
-public class AlipayConfig{
+public class AlipayConfig implements IPayConfig{
 	
 	@Value("${alipay_public_key}")
 	private String privateKey;
