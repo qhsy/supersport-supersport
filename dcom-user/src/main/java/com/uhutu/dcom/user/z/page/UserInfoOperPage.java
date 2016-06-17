279c98b19e0fe8d6bc88563dd2ad540d2c3131ba
xiegj <xiegj@ichsy.com>
2016-06-17 11:04:33 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UserInfoOperPage.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UserInfoOperPage.java
@@ -15,7 +15,8 @@ import com.uhutu.zooweb.define.DefineZooWeb;
  */
 @ZooPage(name = "用户相关操作", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
 		DefineWebPage.Page_Query + "=dzoi4699100110171001",
-		DefineWebPage.Page_Grid + "=dzoi41101001" }, deploy = { DefineWebDeploy.Url_Query + "=code" })
+		DefineWebPage.Page_Grid + "=dzoi4699100110091001,dzoi41101001" }, deploy = {
+				DefineWebDeploy.Url_Query + "=code" })
 public class UserInfoOperPage extends RootPageSimple<UcUserinfo> {
 
 }
