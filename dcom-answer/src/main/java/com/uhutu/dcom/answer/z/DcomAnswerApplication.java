3883673e548981cb5638c8ad498258285d610efd
逄小帅 <@DESKTOP-P9ASMJO>
2016-06-24 15:23:27 +0800

    x

---
+++ dcom-answer/src/main/java/com/uhutu/dcom/answer/z/DcomAnswerApplication.java
@@ -0,0 +1,12 @@
+package com.uhutu.dcom.answer.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcomAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcomAnswerApplication.class, args);
+	}
+}
