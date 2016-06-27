86fc55e1f57a8afe29bea55bc9675af967b15573
xiegj <xiegj@ichsy.com>
2016-06-27 11:17:07 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiForLogin.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiForLogin.java
@@ -1,27 +1,19 @@
 package com.uhutu.sportcenter.z.api.user;
 
 import java.util.Date;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.uhutu.dcom.user.z.entity.UcUserinfo;
-import com.uhutu.dcom.user.z.entity.UcUserinfoExt;
 import com.uhutu.dcom.user.z.enums.UserEnum;
-import com.uhutu.dcom.user.z.properties.ConfigDcomUser;
 import com.uhutu.dcom.user.z.service.UserServiceFactory;
 import com.uhutu.dcom.user.z.support.TecentSigSupport;
-import com.uhutu.dcom.user.z.tecent.base.TecentJoinBase;
-import com.uhutu.dcom.user.z.tecent.entity.AccountImport;
-import com.uhutu.dcom.user.z.tecent.entity.MsgBody;
-import com.uhutu.dcom.user.z.tecent.entity.MsgInfo;
-import com.uhutu.dcom.user.z.tecent.entity.TIMTextElem;
-import com.uhutu.dcom.user.z.tecent.enumer.MsgTypeEnum;
-import com.uhutu.dcom.user.z.tecent.result.TecentResult;
 import com.uhutu.sportcenter.z.input.ApiForLoginInput;
 import com.uhutu.sportcenter.z.result.ApiForLoginResult;
 import com.uhutu.zoocom.define.DefineUser;
 import com.uhutu.zoocom.root.RootApiBase;
-import com.uhutu.zoodata.z.helper.JdbcHelper;
 import com.uhutu.zooweb.user.UserCallFactory;
 import com.uhutu.zooweb.user.UserLoginInput;
 import com.uhutu.zooweb.user.UserLoginResult;
@@ -82,24 +74,29 @@ public class ApiForLogin extends RootApiBase<ApiForLoginInput, ApiForLoginResult
 
 		}
 		if (StringUtils.isNotBlank(result.getUserCode())) {
-			TecentJoinBase base = new TecentJoinBase();
-			UcUserinfoExt ext = JdbcHelper.queryOne(UcUserinfoExt.class, "userCode", ucUserinfo.getCode());
-			AccountImport accountImport = new AccountImport();
-			accountImport.setFaceUrl(ext.getAboutHead());
-			accountImport.setNick(ext.getNickName());
-			accountImport.setIdentifier(ext.getUserCode());
-			base.postTecent(ConfigDcomUser.upConfig().getLoginSdkAccountImport(), accountImport);
+			// TecentJoinBase base = new TecentJoinBase();
+			// UcUserinfoExt ext = JdbcHelper.queryOne(UcUserinfoExt.class,
+			// "userCode", ucUserinfo.getCode());
+			// AccountImport accountImport = new AccountImport();
+			// accountImport.setFaceUrl(ext.getAboutHead());
+			// accountImport.setNick(ext.getNickName());
+			// accountImport.setIdentifier(ext.getUserCode());
+			// base.postTecent(ConfigDcomUser.upConfig().getLoginSdkAccountImport(),
+			// accountImport);
 
 			result.setSig(new TecentSigSupport().upSigCodeByUserCode(result.getUserCode()));
-			MsgInfo msgInfo = new MsgInfo();
-			msgInfo.setTo_Account(result.getUserCode());
-			MsgBody msgBody = new MsgBody();
-			msgBody.setMsgType(MsgTypeEnum.TIMTextElem.name());
-			TIMTextElem textElem = new TIMTextElem();
-			textElem.setText("您家水表已被查,您的登录手机号为：" + ucUserinfo.getLoginName() + "，用户编号为：" + ucUserinfo.getCode());
-			msgBody.setMsgContent(textElem);
-			msgInfo.getMsgBody()[0] = msgBody;
-			TecentResult tecentResult = base.postTecent(ConfigDcomUser.upConfig().getLoginSdkSendmsg(), msgInfo);
+			// MsgInfo msgInfo = new MsgInfo();
+			// msgInfo.setTo_Account(result.getUserCode());
+			// MsgBody msgBody = new MsgBody();
+			// msgBody.setMsgType(MsgTypeEnum.TIMTextElem.name());
+			// TIMTextElem textElem = new TIMTextElem();
+			// textElem.setText("您家水表已被查,您的登录手机号为：" + ucUserinfo.getLoginName()
+			// + "，用户编号为：" + ucUserinfo.getCode());
+			// msgBody.setMsgContent(textElem);
+			// msgInfo.getMsgBody()[0] = msgBody;
+			// TecentResult tecentResult =
+			// base.postTecent(ConfigDcomUser.upConfig().getLoginSdkSendmsg(),
+			// msgInfo);
 		}
 		return result;
 
