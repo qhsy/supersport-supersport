8178e5946a1585ff3a97e20c0f264b3cfbce7114
xiegj <xiegj@ichsy.com>
2016-07-04 19:15:48 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerQuestion.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerQuestion.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 import com.uhutu.dcom.answer.z.entity.AwQuestionInfo;
 import com.uhutu.sportcenter.z.input.ApiForAnswerQuestionInput;
 import com.uhutu.sportcenter.z.result.ApiForAnswerQuestionResult;
+import com.uhutu.zoocom.helper.DateHelper;
 import com.uhutu.zoocom.root.RootApiToken;
+import com.uhutu.zoodata.z.helper.JdbcHelper;
 
 /**
  * 回答问达信息
@@ -19,7 +21,16 @@ public class ApiForAnswerQuestion extends RootApiToken<ApiForAnswerQuestionInput
 
 		ApiForAnswerQuestionResult result = new ApiForAnswerQuestionResult();
 		AwQuestionInfo qtInfo = new AwQuestionInfo();
-		qtInfo.setStatus("dzsd4888100110010002");
+		qtInfo.setAnswerTime(DateHelper.upNow());
+		qtInfo.setCode(input.getCode());
+		if (input.isRefuse()) {
+			qtInfo.setStatus("dzsd4888100110010003");
+		} else {
+			qtInfo.setStatus("dzsd4888100110010002");
+			qtInfo.setLengh(input.getLengh());
+			qtInfo.setUrl(input.getUrl());
+		}
+		JdbcHelper.update(qtInfo, "status,length,url", "code");
 		return result;
 	}
 
