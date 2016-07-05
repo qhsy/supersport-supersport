c7e432de789150f8fdece8b264b5431f993791c9
xiegj <xiegj@ichsy.com>
2016-07-05 17:34:20 +0800

    x

--- dcom-answer/src/main/java/com/uhutu/dcom/answer/z/support/QuestionSupport.java
+++ dcom-answer/src/main/java/com/uhutu/dcom/answer/z/support/QuestionSupport.java
@@ -3,9 +3,13 @@ package com.uhutu.dcom.answer.z.support;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.uhutu.dcom.answer.z.entity.AwPointRecommen;
+import com.uhutu.dcom.answer.z.entity.AwQuestionInfo;
 import com.uhutu.dcom.answer.z.support.vo.AnswerForShow;
 import com.uhutu.dcom.answer.z.support.vo.QuestionForShow;
+import com.uhutu.zoocom.model.MDataMap;
 import com.uhutu.zoocom.root.RootClass;
+import com.uhutu.zoodata.z.helper.JdbcHelper;
 
 public class QuestionSupport extends RootClass {
 
@@ -20,7 +24,16 @@ public class QuestionSupport extends RootClass {
 	 */
 	public List<QuestionForShow> getHotQuestions(int page, int num) {
 		List<QuestionForShow> result = new ArrayList<QuestionForShow>();
-		
+		if(page>0&&num>0){
+			List<AwPointRecommen> recommens = JdbcHelper.queryForList(AwPointRecommen.class, "", " sort desc ", " type='dzsd4888100110030001'", new MDataMap(),(page-1)*num,num);
+			if(recommens!=null&&!recommens.isEmpty()&&recommens.size()>0){
+				
+			}else {
+				List<AwQuestionInfo>  others = JdbcHelper.queryForList(AwQuestionInfo.class, "", "listen desc", "scope='dzsd4888100110020002' and status='dzsd4888100110010002'", new MDataMap(), 0, 0);
+			}
+			
+			
+		}
 		return result;
 	}
 
