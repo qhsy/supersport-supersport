40430675e565b00e794c4b7099b68e4a6231e999
逄小帅 <@DESKTOP-P9ASMJO>
2016-07-05 13:53:24 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiQuestionInfoListInput.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiQuestionInfoListInput.java
@@ -3,6 +3,7 @@ package com.uhutu.sportcenter.z.input;
 import com.uhutu.zoocom.root.RootApiInput;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * 个人中心我问列表信息
@@ -13,9 +14,11 @@ import io.swagger.annotations.ApiModel;
 public class ApiQuestionInfoListInput extends RootApiInput {
 	
 	/*页码*/
+	@ApiModelProperty(value="当前页码")
 	private int pagination;
 	
 	/*每页数量*/
+	@ApiModelProperty(value="每页展示的数量")
 	private int pageNum;
 
 	public int getPagination() {
