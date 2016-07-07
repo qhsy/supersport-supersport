8a547107e427a2fe1e200974de101ef96fad564d
xiegj <xiegj@ichsy.com>
2016-07-07 14:07:31 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiSportingMomentsInput.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiSportingMomentsInput.java
@@ -6,10 +6,13 @@ import io.swagger.annotations.ApiModelProperty;
 
 public class ApiSportingMomentsInput extends RootApiInput {
 
-	@ApiModelProperty(name="页码",value="页码",example="0")
-	private int  pagination= 0;
-	
-	@ApiModelProperty(value="宽度")
+	@ApiModelProperty(name = "查询类型", required = true, value = "查询类型1：全部。2：我关注的人，3：GODO达人", example = "1")
+	private String type = "1";
+
+	@ApiModelProperty(name = "页码", value = "页码", example = "0")
+	private int pagination = 0;
+
+	@ApiModelProperty(value = "宽度")
 	private int width;
 
 	public int getPagination() {
@@ -27,5 +30,13 @@ public class ApiSportingMomentsInput extends RootApiInput {
 	public void setWidth(int width) {
 		this.width = width;
 	}
-	
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
