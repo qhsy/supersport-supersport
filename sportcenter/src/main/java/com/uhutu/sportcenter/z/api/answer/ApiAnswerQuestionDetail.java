a053517dfe026d65e091bc47871e1ef73f402b89
xiegj <xiegj@ichsy.com>
2016-07-07 16:19:52 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiAnswerQuestionDetail.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiAnswerQuestionDetail.java
@@ -30,18 +30,29 @@ public class ApiAnswerQuestionDetail extends RootApiToken<ApiAnswerQuestionDetai
 		if (StringUtils.isNotBlank(input.getCode())) {
 			AwQuestionInfo questionInfo = JdbcHelper.queryOne(AwQuestionInfo.class, "code", input.getCode());
 			if (questionInfo != null) {
-				UcUserinfoExt ext = JdbcHelper.queryOne(UcUserinfoExt.class, "user_code",
-						questionInfo.getQuestionUserCode());
-				UcUserinfo info = JdbcHelper.queryOne(UcUserinfo.class, "code", questionInfo.getQuestionUserCode());
-				result.getShow().setAnswerCode(questionInfo.getCode());
-				result.getShow().setAskMoney(questionInfo.getMoney());
-				result.getShow().setContent(questionInfo.getContent());
-				result.getShow().setHeadUrl(ext.getAboutHead());
-				result.getShow().setNickName(ext.getNickName());
-				result.getShow().setScope(questionInfo.getScope());
-				result.getShow().setType(info.getType());
-				result.getShow().setUserCode(questionInfo.getQuestionUserCode());
-				result.getShow().setTimeShow(getDistanceTime(questionInfo.getZc(), 48));
+				if ("dzsd4888100110010001".equals(questionInfo.getStatus())) {
+					if ("dzsd4888100110020002".equals(questionInfo.getScope())
+							|| (upUserCode().equals(questionInfo.getQuestionUserCode())
+									|| upUserCode().equals(questionInfo.getAnswerUserCode()))) {
+						UcUserinfoExt ext = JdbcHelper.queryOne(UcUserinfoExt.class, "user_code",
+								questionInfo.getQuestionUserCode());
+						UcUserinfo info = JdbcHelper.queryOne(UcUserinfo.class, "code",
+								questionInfo.getQuestionUserCode());
+						result.getShow().setAnswerCode(questionInfo.getCode());
+						result.getShow().setAskMoney(questionInfo.getMoney());
+						result.getShow().setContent(questionInfo.getContent());
+						result.getShow().setHeadUrl(ext.getAboutHead());
+						result.getShow().setNickName(ext.getNickName());
+						result.getShow().setScope(questionInfo.getScope());
+						result.getShow().setType(info.getType());
+						result.getShow().setUserCode(questionInfo.getQuestionUserCode());
+						result.getShow().setTimeShow(getDistanceTime(questionInfo.getZc(), 2));
+					} else {
+						result.inError(88880010);
+					}
+				} else {
+					result.inError(88880009);
+				}
 			} else {
 				result.inError(88880008);
 			}
@@ -49,25 +60,22 @@ public class ApiAnswerQuestionDetail extends RootApiToken<ApiAnswerQuestionDetai
 		return result;
 	}
 
-	public static String getDistanceTime(Date one, int hours) {
+	public static String getDistanceTime(Date one, int dayNum) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(one);
-		calendar.add(Calendar.HOUR_OF_DAY, hours);
-		Date two = calendar.getTime();
-		long day = 0;
+		calendar.add(Calendar.DAY_OF_MONTH, dayNum);
 		long hour = 0;
 		long min = 0;
-		long time1 = one.getTime();
-		long time2 = two.getTime();
+		long time1 = new Date().getTime();
+		long time2 = calendar.getTime().getTime();
 		long diff;
 		if (time1 < time2) {
 			diff = time2 - time1;
 		} else {
 			diff = time1 - time2;
 		}
-		day = diff / (24 * 60 * 60 * 1000);
-		hour = (diff / (60 * 60 * 1000) - day * 24);
-		min = ((diff / (60 * 1000)) - day * 24 * 60 - hour * 60);
+		hour = (diff / (60 * 60 * 1000));
+		min = ((diff / (60 * 1000)) - hour * 60);
 		return "还有" + hour + "小时" + min + "分过期";
 	}
 }
