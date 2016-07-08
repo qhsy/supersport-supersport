42a4d0fa99a857edd5127671c679ed949d202252
xiegj <xiegj@ichsy.com>
2016-07-08 10:42:50 +0800

    x

--- dcom-order/src/main/java/com/uhutu/dcom/order/make/TeslaCheckAnswerActivity.java
+++ dcom-order/src/main/java/com/uhutu/dcom/order/make/TeslaCheckAnswerActivity.java
@@ -1,13 +1,15 @@
 package com.uhutu.dcom.order.make;
 
+import java.math.BigDecimal;
+
 import com.uhutu.dcom.activity.z.entity.AcActivityAnswerInfo;
 import com.uhutu.dcom.activity.z.support.AnswerActivitySupport;
-import com.uhutu.dcom.answer.z.entity.AwQuestionInfo;
 import com.uhutu.dcom.order.orderResult.TeslaXOrder;
 import com.uhutu.dcom.order.orderResult.TeslaXResult;
 import com.uhutu.dcom.order.top.TeslaTopOrderMake;
 import com.uhutu.dcom.order.z.entity.OcOrderActivity;
 import com.uhutu.zoocom.helper.TopHelper;
+import com.uhutu.zoocom.model.MDataMap;
 import com.uhutu.zoodata.z.helper.JdbcHelper;
 
 /**
@@ -20,19 +22,21 @@ public class TeslaCheckAnswerActivity extends TeslaTopOrderMake {
 
 	public TeslaXResult doRefresh(TeslaXOrder teslaOrder) {
 		TeslaXResult result = new TeslaXResult();
-		AwQuestionInfo questionInfo = JdbcHelper.queryOne(AwQuestionInfo.class, "code",
+		MDataMap questionInfo = JdbcHelper.dataOne("aw_question_info", "code",
 				teslaOrder.getDetail().get(0).getProductCode());
 		if (questionInfo == null) {
 			result.setStatus(81120002);
 			result.setError(TopHelper.upInfo(81120002));
 		} else {
-			teslaOrder.getDetail().get(0).setProductName(questionInfo.getContent());
+			teslaOrder.getDetail().get(0).setProductName(questionInfo.get("content"));
 			if ("dzsd4112100110010003".equals(teslaOrder.getOrderInfo().getOrderType())) {
-				teslaOrder.getDetail().get(0).setProductPrice(questionInfo.getMoney());
+				teslaOrder.getDetail().get(0)
+						.setProductPrice(BigDecimal.valueOf(Double.valueOf(questionInfo.get("money"))));
 			} else if ("dzsd4112100110010004".equals(teslaOrder.getOrderInfo().getOrderType())) {
-				teslaOrder.getDetail().get(0).setProductPrice(questionInfo.getSellMoney());
+				teslaOrder.getDetail().get(0)
+						.setProductPrice(BigDecimal.valueOf(Double.valueOf(questionInfo.get("sell_money"))));
 			}
-			teslaOrder.getOrderInfo().setSellerCode(questionInfo.getQuestionUserCode());
+			teslaOrder.getOrderInfo().setSellerCode(questionInfo.get("question_user_code"));
 			AcActivityAnswerInfo activityInfo = new AnswerActivitySupport()
 					.getActivityInfoByAnswerCode(teslaOrder.getDetail().get(0).getCode());
 			if (activityInfo != null) {
@@ -40,8 +44,8 @@ public class TeslaCheckAnswerActivity extends TeslaTopOrderMake {
 				OcOrderActivity activity = new OcOrderActivity();
 				activity.setActivityCode(activityInfo.getCode());
 				activity.setProductActivityPrice(activityInfo.getPrice());
-				activity.setProductCode(questionInfo.getCode());
-				activity.setProductPrice(questionInfo.getSellMoney());
+				activity.setProductCode(questionInfo.get("code"));
+				activity.setProductPrice(BigDecimal.valueOf(Double.valueOf(questionInfo.get("sell_money"))));
 				teslaOrder.getActivitys().add(activity);
 			}
 		}
