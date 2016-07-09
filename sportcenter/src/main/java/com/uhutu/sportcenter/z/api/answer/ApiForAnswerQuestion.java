5b82bcbac505bc393a2faf28f6bb0aed8fb08332
逄小帅 <@DESKTOP-P9ASMJO>
2016-07-09 10:40:10 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerQuestion.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerQuestion.java
@@ -1,11 +1,21 @@
 package com.uhutu.sportcenter.z.api.answer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.uhutu.dcom.answer.z.entity.AwQuestionInfo;
+import com.uhutu.dcom.answer.z.service.AnswerServiceFactory;
+import com.uhutu.dcom.pay.z.common.PayProcessEnum;
+import com.uhutu.dcom.pay.z.request.WechatMsgAnswerRequest;
+import com.uhutu.dcom.pay.z.response.WechatMsgResponse;
+import com.uhutu.dcom.pay.z.util.WechatMsgComponent;
+import com.uhutu.dcom.user.z.entity.UcUserinfoExt;
+import com.uhutu.dcom.user.z.entity.UcUserinfoSocial;
+import com.uhutu.dcom.user.z.support.UserInfoSupport;
 import com.uhutu.sportcenter.z.input.ApiForAnswerQuestionInput;
 import com.uhutu.sportcenter.z.result.ApiForAnswerQuestionResult;
 import com.uhutu.zoocom.helper.DateHelper;
+import com.uhutu.zoocom.helper.TopHelper;
 import com.uhutu.zoocom.root.RootApiToken;
 import com.uhutu.zoodata.z.helper.JdbcHelper;
 
@@ -15,23 +25,86 @@ import com.uhutu.zoodata.z.helper.JdbcHelper;
  */
 @Component
 public class ApiForAnswerQuestion extends RootApiToken<ApiForAnswerQuestionInput, ApiForAnswerQuestionResult> {
+	
+	@Autowired
+	private UserInfoSupport userInfoSupport;
+	
+	@Autowired
+	private WechatMsgComponent wechatMsgCompoent;
+	
+	@Autowired
+	private AnswerServiceFactory answerServiceFactory;
 
 	@Override
 	protected ApiForAnswerQuestionResult process(ApiForAnswerQuestionInput input) {
 
 		ApiForAnswerQuestionResult result = new ApiForAnswerQuestionResult();
-		AwQuestionInfo qtInfo = new AwQuestionInfo();
-		qtInfo.setAnswerTime(DateHelper.upNow());
-		qtInfo.setCode(input.getCode());
-		if (input.isRefuse()) {
-			qtInfo.setStatus("dzsd4888100110010003");
-		} else {
-			qtInfo.setStatus("dzsd4888100110010002");
-			qtInfo.setLengh(input.getLengh());
-			qtInfo.setUrl(input.getUrl());
+		AwQuestionInfo qtInfo = answerServiceFactory.getQuestionInfoService().queryByCode(input.getCode());
+	
+		if(qtInfo != null){
+			
+			qtInfo.setAnswerTime(DateHelper.upNow());
+			qtInfo.setCode(input.getCode());
+			if (input.isRefuse()) {
+				qtInfo.setStatus("dzsd4888100110010003");
+			} else {
+				qtInfo.setStatus("dzsd4888100110010002");
+				qtInfo.setLengh(input.getLengh());
+				qtInfo.setUrl(input.getUrl());
+				
+				WechatMsgResponse wechatMsgResponse = sendWechatMsg(qtInfo);
+				
+				if(!wechatMsgResponse.upFlag()){
+					
+					result.setStatus(0);
+					
+					result.setError(wechatMsgResponse.getErrmsg());
+					
+				}
+				
+			}
+			
+			if(result.upFlagTrue()){
+				
+				JdbcHelper.update(qtInfo, "status,length,url", "code");
+				
+			}
+			
+		}else{
+			
+			result.inError(88880022);
+			
 		}
-		JdbcHelper.update(qtInfo, "status,length,url", "code");
+		
 		return result;
+	}
+	
+	/**
+	 * 发送微信消息
+	 * @param questionInfo
+	 * 		问题信息
+	 * @return 信息响应
+	 */
+	public WechatMsgResponse sendWechatMsg(AwQuestionInfo questionInfo){
+		
+		WechatMsgAnswerRequest answerRequest = new WechatMsgAnswerRequest();
+		
+		UcUserinfoExt ucUserinfoExt = userInfoSupport.getUserInfoExt(upUserCode());
+		
+		UcUserinfoSocial ucUserinfoSocial = userInfoSupport.getUserInfoSocial(questionInfo.getQuestionUserCode());
+		
+		answerRequest.getFirst().setValue(TopHelper.upInfo(88880019, ucUserinfoExt.getNickName()));
+		
+		answerRequest.getKeyword1().setValue(ucUserinfoExt.getNickName());
+		
+		answerRequest.getKeyword2().setValue(questionInfo.getAnswerTime());
+		
+		answerRequest.getKeyword3().setValue(TopHelper.upInfo(88880020, questionInfo.getLengh()));
+		
+		answerRequest.getRemark().setValue(TopHelper.upInfo(88880021));
+		
+		return wechatMsgCompoent.sendMsg(ucUserinfoSocial.getAccountId(), "",PayProcessEnum.WECHAT_MSG_ASK, answerRequest);
+		
 	}
 
 }
