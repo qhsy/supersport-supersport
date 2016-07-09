b3e525219b654f2c402c8ba2aaf2aa174e5dbaa5
逄小帅 <@DESKTOP-P9ASMJO>
2016-07-09 17:01:41 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiAnswerUserInfo.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiAnswerUserInfo.java
@@ -43,7 +43,7 @@ public class ApiAnswerUserInfo extends RootApiToken<ApiAnswerUserInfoInput, ApiA
 			
 		}else{
 			
-			answerUserInfo.setStatus(SystemEnum.YES.getCode());
+			answerUserInfo.setStatus(SystemEnum.NO.getCode());
 			
 		}
 
