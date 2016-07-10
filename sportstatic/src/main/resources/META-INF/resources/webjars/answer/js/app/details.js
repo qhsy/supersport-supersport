17a32d17f4be233acf39958bcff92a04249fe9cd
逄小帅 <@DESKTOP-P9ASMJO>
2016-07-10 20:36:04 +0800

    x

--- sportstatic/src/main/resources/META-INF/resources/webjars/answer/js/app/details.js
+++ sportstatic/src/main/resources/META-INF/resources/webjars/answer/js/app/details.js
@@ -46,70 +46,96 @@ require(['zepto','vue','common','jssdk','extend'],function($,Vue,comm,wx){
 		methods:{
 			audioPlay:function(){
 				var self = this;
-				if(self.result.detail.listenFlag){
-					audioPlay();
-				}else{
-					if (typeof WeixinJSBridge == "undefined"){
-						if( document.addEventListener ){
-							document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-						} else if (document.attachEvent){
-							document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
-							document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+				$.ajax({
+					url:'/api/answerController/playAudio',
+					type:'POST',
+					contentType:'application/json',
+					dataType:'json',
+					async:false,
+					data:'{"audioUrl": "' + self.result.videoUrl + '","questionCode": "' + code + '","zoo": {"key": "tesetkey","token": ""}}',
+					success:function(res){
+						if(res.status == 1){
+							wx.downloadVoice({
+								serverId: res.mediaId, // 需要下载的音频的服务器端ID，由uploadVoice接口获得
+								success: function (res) {
+									localId = res.localId; // 返回音频的本地ID
+								}
+							});
 						}
-					} else {
-						onBridgeReady();
 					}
-				}
-				function onBridgeReady(){
-					$.ajax({
-						url:'/api/payController/wechatH5Pay',
-						type:'POST',
-						contentType:'application/json',
-						dataType:'json',
-						async:false,
-						data:'{"questionCode": "' + code + '","romoteIP": "string","serveIP": "string","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
-						success:function(res){
-							if(res.status == 1){
-								getBrandWCPayRequest = res.wechatH5PayInfo;
-							}
-						}
-					});
-					WeixinJSBridge.invoke('getBrandWCPayRequest',getBrandWCPayRequest,function(res){
-						if(res.err_msg == "get_brand_wcpay_request：ok" ) {
-							self.result.detail.listenFlag = true;
-							self.result.detail.videoShow = '';
-							audioPlay();
-						}     
-					}); 
-				}
-				function play(){
-					$.ajax({
-						url:'/api/answerController/playAudio',
-						type:'POST',
-						contentType:'application/json',
-						dataType:'json',
-						async:false,
-						data:'{"audioUrl": "' + self.result.videoUrl + '","questionCode": "' + code + '","zoo": {"key": "tesetkey","token": ""}}',
-						success:function(res){
-							if(res.status == 1){
-								wx.downloadVoice({
-									serverId: res.mediaId, // 需要下载的音频的服务器端ID，由uploadVoice接口获得
-									success: function (res) {
-										localId = res.localId; // 返回音频的本地ID
-									}
-								});
-							}
-						}
-					});
-					wx.playVoice({
-				        localId: localId
-				    });
-				    wx.onVoicePlayEnd({
-					    complete: function(res) {
-					        console.log('播放结束');
-					    }
-					});
-				}
+				});
+				wx.playVoice({
+			        localId: localId
+			    });
+			    wx.onVoicePlayEnd({
+				    complete: function(res) {
+				        console.log('播放结束');
+				    }
+				});
+				// if(self.result.detail.listenFlag){
+				// 	audioPlay();
+				// }else{
+				// 	if (typeof WeixinJSBridge == "undefined"){
+				// 		if( document.addEventListener ){
+				// 			document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+				// 		} else if (document.attachEvent){
+				// 			document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+				// 			document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+				// 		}
+				// 	} else {
+				// 		onBridgeReady();
+				// 	}
+				// }
+				// function onBridgeReady(){
+				// 	$.ajax({
+				// 		url:'/api/payController/wechatH5Pay',
+				// 		type:'POST',
+				// 		contentType:'application/json',
+				// 		dataType:'json',
+				// 		async:false,
+				// 		data:'{"questionCode": "' + code + '","romoteIP": "string","serveIP": "string","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
+				// 		success:function(res){
+				// 			if(res.status == 1){
+				// 				getBrandWCPayRequest = res.wechatH5PayInfo;
+				// 			}
+				// 		}
+				// 	});
+				// 	WeixinJSBridge.invoke('getBrandWCPayRequest',getBrandWCPayRequest,function(res){
+				// 		if(res.err_msg == "get_brand_wcpay_request：ok" ) {
+				// 			self.result.detail.listenFlag = true;
+				// 			self.result.detail.videoShow = '';
+				// 			audioPlay();
+				// 		}     
+				// 	}); 
+				// }
+				// function play(){
+				// 	$.ajax({
+				// 		url:'/api/answerController/playAudio',
+				// 		type:'POST',
+				// 		contentType:'application/json',
+				// 		dataType:'json',
+				// 		async:false,
+				// 		data:'{"audioUrl": "' + self.result.videoUrl + '","questionCode": "' + code + '","zoo": {"key": "tesetkey","token": ""}}',
+				// 		success:function(res){
+				// 			if(res.status == 1){
+				// 				wx.downloadVoice({
+				// 					serverId: res.mediaId, // 需要下载的音频的服务器端ID，由uploadVoice接口获得
+				// 					success: function (res) {
+				// 						localId = res.localId; // 返回音频的本地ID
+				// 					}
+				// 				});
+				// 			}
+				// 		}
+				// 	});
+				// 	wx.playVoice({
+				//         localId: localId
+				//     });
+				//     wx.onVoicePlayEnd({
+				// 	    complete: function(res) {
+				// 	        console.log('播放结束');
+				// 	    }
+				// 	});
+				// }
 
 			}
 				
