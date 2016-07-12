cb27fdfb3f795dd0a08ddccfc2253ca5cacc07a8
xiegj <xiegj@ichsy.com>
2016-07-12 16:28:36 +0800

    x

--- sportstatic/src/main/resources/META-INF/resources/webjars/answer/js/app/details.js
+++ sportstatic/src/main/resources/META-INF/resources/webjars/answer/js/app/details.js
@@ -2,7 +2,8 @@ require(['zepto','vue','common','jssdk','extend'],function($,Vue,comm,wx){
 	var code = comm.paramFn('id');
 	var localId;
 	var getBrandWCPayRequest = null;
-	var details = new Vue({
+	var status;
+	var vm = new Vue({
 		el: '#details',
 		data: {
 			result:{}
@@ -46,97 +47,62 @@ require(['zepto','vue','common','jssdk','extend'],function($,Vue,comm,wx){
 		methods:{
 			audioPlay:function(){
 				var self = this;
-				$.ajax({
-					url:'/api/answerController/playAudio',
-					type:'POST',
-					contentType:'application/json',
-					dataType:'json',
-					async:false,
-					data:'{"audioUrl": "' + self.result.detail.videoUrl + '","questionCode": "' + code + '","zoo": {"key": "tesetkey","token": ""}}',
-					success:function(res){
-						if(res.status == 1){
-							wx.downloadVoice({
-								serverId: res.mediaId, // 需要下载的音频的服务器端ID，由uploadVoice接口获得
-								success: function (res) {
-									localId = res.localId; // 返回音频的本地ID
-									wx.playVoice({
-								        localId: localId
-								    });
-								    wx.onVoicePlayEnd({
-									    complete: function(res) {
-									        console.log('播放结束');
-									    }
-									});
-								}
-							});
+				var code = self.result.detail.answerUserCode;
+				if(self.result.detail.listenFlag){
+					play();
+				}else{
+					$.ajax({
+						url:'/api/orderController/answerOrder',
+						type:'POST',
+						contentType:'application/json',
+						dataType:'json',
+						async:false,
+						data:'{"answerCode": "' + code + '","appVersion": "","orderSource": "dzsd4112100110020002","orderType": "dzsd4112100110010004","payType": "dzsd4112100110040002","romoteIP": "","serveIP": "","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
+						success:function(res){
+							if(res.status == 1){
+								wx.chooseWXPay({
+								    timestamp: res.wechatH5PayResponse.timeStamp,
+								    nonceStr: res.wechatH5PayResponse.nonceStr,
+								    package: 'prepay_id=' + res.wechatH5PayResponse.prepay_id,
+								    signType: res.wechatH5PayResponse.signType,
+								    paySign: res.wechatH5PayResponse.paySign,
+								    success: function (data) {
+								    	self.result.detail.listenFlag = true;
+								    	self.result.detail.videoShow = '';
+								    	play();
+								    }
+								});
+							}
 						}
-					}
-				});
-				
-				// if(self.result.detail.listenFlag){
-				// 	audioPlay();
-				// }else{
-				// 	if (typeof WeixinJSBridge == "undefined"){
-				// 		if( document.addEventListener ){
-				// 			document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-				// 		} else if (document.attachEvent){
-				// 			document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
-				// 			document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-				// 		}
-				// 	} else {
-				// 		onBridgeReady();
-				// 	}
-				// }
-				// function onBridgeReady(){
-				// 	$.ajax({
-				// 		url:'/api/payController/wechatH5Pay',
-				// 		type:'POST',
-				// 		contentType:'application/json',
-				// 		dataType:'json',
-				// 		async:false,
-				// 		data:'{"questionCode": "' + code + '","romoteIP": "string","serveIP": "string","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
-				// 		success:function(res){
-				// 			if(res.status == 1){
-				// 				getBrandWCPayRequest = res.wechatH5PayInfo;
-				// 			}
-				// 		}
-				// 	});
-				// 	WeixinJSBridge.invoke('getBrandWCPayRequest',getBrandWCPayRequest,function(res){
-				// 		if(res.err_msg == "get_brand_wcpay_request：ok" ) {
-				// 			self.result.detail.listenFlag = true;
-				// 			self.result.detail.videoShow = '';
-				// 			audioPlay();
-				// 		}     
-				// 	}); 
-				// }
-				// function play(){
-				// 	$.ajax({
-				// 		url:'/api/answerController/playAudio',
-				// 		type:'POST',
-				// 		contentType:'application/json',
-				// 		dataType:'json',
-				// 		async:false,
-				// 		data:'{"audioUrl": "' + self.result.videoUrl + '","questionCode": "' + code + '","zoo": {"key": "tesetkey","token": ""}}',
-				// 		success:function(res){
-				// 			if(res.status == 1){
-				// 				wx.downloadVoice({
-				// 					serverId: res.mediaId, // 需要下载的音频的服务器端ID，由uploadVoice接口获得
-				// 					success: function (res) {
-				// 						localId = res.localId; // 返回音频的本地ID
-				// 					}
-				// 				});
-				// 			}
-				// 		}
-				// 	});
-				// 	wx.playVoice({
-				//         localId: localId
-				//     });
-				//     wx.onVoicePlayEnd({
-				// 	    complete: function(res) {
-				// 	        console.log('播放结束');
-				// 	    }
-				// 	});
-				// }
+					});
+				}
+				function play(){
+					$.ajax({
+						url:'/api/answerController/playAudio',
+						type:'POST',
+						contentType:'application/json',
+						dataType:'json',
+						async:false,
+						data:'{"audioUrl": "' + self.result.detail.videoUrl + '","questionCode": "' + code + '","zoo": {"key": "tesetkey","token": ""}}',
+						success:function(res){
+							if(res.status == 1){
+								wx.downloadVoice({
+									serverId: res.mediaId,
+									success: function (res) {
+										localId = res.localId; 
+										wx.playVoice({
+									        localId: localId
+									    });
+									    wx.onVoicePlayEnd({
+										    complete: function(res) {
+										    }
+										});
+									}
+								});
+							}
+						}
+					});
+				}
 
 			},
 			assistFn:function(){
