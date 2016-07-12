9f218efa55e56a2e5d260ebe2c2cae6aeeb7d733
xiegj <xiegj@ichsy.com>
2016-07-12 09:47:55 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiForAnswerOrderInput.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiForAnswerOrderInput.java
@@ -11,13 +11,13 @@ public class ApiForAnswerOrderInput extends RootApiInput {
 	@ApiModelProperty(value = "问题编号", notes = "问达问题编号", example = "WTBH0001", required = true)
 	private String answerCode = "";
 
-	@ApiModelProperty(value = "问答类型", notes = "dzsd4112100110010003:问达订单,dzsd4112100110010004:偷听订单", example = "dzsd4112100110010004", required = true)
+	@ApiModelProperty(value = "问答类型 dzsd4112100110010003:问达订单,dzsd4112100110010004:偷听订单", example = "dzsd4112100110010004", required = true)
 	private String orderType;
 
-	@ApiModelProperty(value = "来源", notes = "dzsd4112100110020001:app订单,dzsd4112100110020002:wap订单", example = "dzsd4112100110020002", required = true)
+	@ApiModelProperty(value = "来源 dzsd4112100110020001:app订单,dzsd4112100110020002:wap订单", example = "dzsd4112100110020002", required = true)
 	private String orderSource;
 
-	@ApiModelProperty(value = "支付类型", notes = "dzsd4112100110040001:支付宝,dzsd4112100110040002:微信", example = "dzsd4112100110040002", required = true)
+	@ApiModelProperty(value = "支付类型  dzsd4112100110040001:支付宝,dzsd4112100110040002:微信", example = "dzsd4112100110040002", required = true)
 	private String payType;
 
 	@ApiModelProperty(value = "app版本信息")
