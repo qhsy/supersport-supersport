f7f80b7813fcf4099249b589310c5d6af0cbb8c9
逄小帅 <@DESKTOP-P9ASMJO>
2016-07-13 14:40:19 +0800

    x

--- sportstatic/src/main/resources/META-INF/resources/webjars/answer/js/app/quiz.js
+++ sportstatic/src/main/resources/META-INF/resources/webjars/answer/js/app/quiz.js
@@ -7,7 +7,8 @@ require(['zepto','vue','common','jssdk','extend'],function($,Vue,comm,wx){
 			content:'',
 			overt:true,
 			num:0,
-			result:{}
+			result:{},
+			isSubmit:true
 		},
 		created:function(){
 			var self = this;
@@ -61,41 +62,44 @@ require(['zepto','vue','common','jssdk','extend'],function($,Vue,comm,wx){
 					comm.tost('输入的内容超过限制！')
 					return false;
 				}
-				$.ajax({
-					url:'/api/answerController/saveQuestion',
-					type:'POST',
-					contentType:'application/json',
-					dataType:'json',
-					async:false,
-					data:'{"answerUserCode": "' + userCode + '","content": "' + self.content + '","orderSource": "dzsd4112100110020002","romoteIP": "","scope": "' + self.overt + '","serveIP": "","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
-					success:function(res){
-						if(res.status == 1){
-							var that = res;
-							wx.chooseWXPay({
-							    timestamp: res.wechatH5PayResponse.timeStamp,
-							    nonceStr: res.wechatH5PayResponse.nonceStr, // 支付签名随机串，不长于 32 位
-							    package: 'prepay_id=' + res.wechatH5PayResponse.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-							    signType: res.wechatH5PayResponse.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-							    paySign: res.wechatH5PayResponse.paySign, // 支付签名
-							    success: function (data) {
-							    	$.ajax({
-										url:'/api/answerController/sendAskWxMsg',
-										type:'POST',
-										contentType:'application/json',
-										dataType:'json',
-										async:false,
-										data:'{"questionCode": "' + that.code + '","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
-										success:function(res){
-											if(res.status == 1){
-												window.location.href = 'details.html?id=' + that.code;
+				if(self.isSubmit){
+					self.isSubmit = false;
+					$.ajax({
+						url:'/api/answerController/saveQuestion',
+						type:'POST',
+						contentType:'application/json',
+						dataType:'json',
+						async:false,
+						data:'{"answerUserCode": "' + userCode + '","content": "' + self.content + '","orderSource": "dzsd4112100110020002","romoteIP": "","scope": "' + self.overt + '","serveIP": "","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
+						success:function(res){
+							if(res.status == 1){
+								var that = res;
+								wx.chooseWXPay({
+								    timestamp: res.wechatH5PayResponse.timeStamp,
+								    nonceStr: res.wechatH5PayResponse.nonceStr, // 支付签名随机串，不长于 32 位
+								    package: 'prepay_id=' + res.wechatH5PayResponse.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+								    signType: res.wechatH5PayResponse.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+								    paySign: res.wechatH5PayResponse.paySign, // 支付签名
+								    success: function (data) {
+								    	$.ajax({
+											url:'/api/answerController/sendAskWxMsg',
+											type:'POST',
+											contentType:'application/json',
+											dataType:'json',
+											async:false,
+											data:'{"questionCode": "' + that.code + '","zoo": {"key": "tesetkey","token": "' + comm.token() + '"}}',
+											success:function(res){
+												if(res.status == 1){
+													window.location.href = 'details.html?id=' + that.code;
+												}
 											}
-										}
-									});
-							    }
-							});
+										});
+								    }
+								});
+							}
 						}
-					}
-				});
+					});
+				}
 			}
 		}
 	});
