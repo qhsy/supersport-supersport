92b3db81ddc468daeb04d31d16739644d34afe57
逄小帅 <@DESKTOP-P9ASMJO>
2016-07-18 15:17:14 +0800

    翘臀大赛

---
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiButtockPowerListInput.java
@@ -0,0 +1,27 @@
+package com.uhutu.sportcenter.z.input;
+
+import com.uhutu.zoocom.root.RootApiInput;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * 翘臀大赛
+ * @author 逄小帅
+ *
+ */
+@ApiModel
+public class ApiButtockPowerListInput extends RootApiInput {
+	
+	@ApiModelProperty(value="当前页码")
+	private int pagination;
+
+	public int getPagination() {
+		return pagination;
+	}
+
+	public void setPagination(int pagination) {
+		this.pagination = pagination;
+	}
+
+}
