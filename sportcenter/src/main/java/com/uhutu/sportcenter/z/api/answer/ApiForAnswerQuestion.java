db732eba723e665f683f8da1e2da69370abf8497
xiegj <xiegj@ichsy.com>
2016-07-22 14:23:29 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerQuestion.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerQuestion.java
@@ -13,6 +13,7 @@ import com.uhutu.dcom.answer.z.entity.AwAnswerRefundJob;
 import com.uhutu.dcom.answer.z.entity.AwQuestionInfo;
 import com.uhutu.dcom.answer.z.service.AnswerServiceFactory;
 import com.uhutu.dcom.config.enums.SocialEnum;
+import com.uhutu.dcom.order.z.entity.OcOrderInfo;
 import com.uhutu.dcom.pay.z.common.PayProcessEnum;
 import com.uhutu.dcom.pay.z.process.impl.PayGateProcess;
 import com.uhutu.dcom.pay.z.request.WechatMsgAnswerRequest;
@@ -61,19 +62,19 @@ public class ApiForAnswerQuestion extends RootApiToken<ApiForAnswerQuestionInput
 		ApiForAnswerQuestionResult result = new ApiForAnswerQuestionResult();
 		AwQuestionInfo qtInfo = answerServiceFactory.getQuestionInfoService().queryByCode(input.getCode());
 		if (StringUtils.isNotBlank(input.getWechatVoiceId())) {
-			
+
 			String key = AnswerEnum.REDIS_AUDIO.getText() + input.getCode();
 
 			IKvCall redisMediaId = KvHelper.upFactory(key);
-				
-			if(!redisMediaId.exists(key)){
-				
+
+			if (!redisMediaId.exists(key)) {
+
 				redisMediaId.set(key, input.getWechatVoiceId());
-				
+
 				redisMediaId.expire(key, Integer.valueOf(AnswerEnum.REDIS_AUDIO.getCode()));
-				
-			}		
-			
+
+			}
+
 			input.setUrl(getWechatVoiceUrl(input.getWechatVoiceId()));
 		}
 		if (qtInfo != null) {
@@ -81,12 +82,12 @@ public class ApiForAnswerQuestion extends RootApiToken<ApiForAnswerQuestionInput
 			qtInfo.setCode(input.getCode());
 			if (input.isRefuse()) {
 				qtInfo.setStatus("dzsd4888100110010003");
-				sentToRefund(qtInfo.getQuestionUserCode(), qtInfo.getCode(), qtInfo.getMoney());
+				sentToRefund(qtInfo);
 			} else {
 				qtInfo.setStatus("dzsd4888100110010002");
 				qtInfo.setLengh(input.getLengh());
 				qtInfo.setUrl(input.getUrl());
-				WechatMsgResponse wechatMsgResponse = sendWechatMsg(qtInfo,input.getRequestUrl());
+				WechatMsgResponse wechatMsgResponse = sendWechatMsg(qtInfo, input.getRequestUrl());
 				if (!wechatMsgResponse.upFlag()) {
 					result.setStatus(0);
 					result.setError(wechatMsgResponse.getErrmsg());
@@ -108,12 +109,12 @@ public class ApiForAnswerQuestion extends RootApiToken<ApiForAnswerQuestionInput
 	 *            问题信息
 	 * @return 信息响应
 	 */
-	public WechatMsgResponse sendWechatMsg(AwQuestionInfo questionInfo,String requestUrl) {
+	public WechatMsgResponse sendWechatMsg(AwQuestionInfo questionInfo, String requestUrl) {
 
 		WechatMsgAnswerRequest answerRequest = new WechatMsgAnswerRequest();
-		
-		requestUrl = requestUrl+"/webjars/answer/details.html?id="+questionInfo.getCode();
-		
+
+		requestUrl = requestUrl + "/webjars/answer/details.html?id=" + questionInfo.getCode();
+
 		requestUrl = TopHelper.upInfo(81110010, requestUrl);
 
 		UcUserinfoExt ucUserinfoExt = userInfoSupport.getUserInfoExt(upUserCode());
@@ -162,20 +163,32 @@ public class ApiForAnswerQuestion extends RootApiToken<ApiForAnswerQuestionInput
 	}
 
 	// 拒绝回答后生成退款任务
-	private void sentToRefund(String userCode, String questionCode, BigDecimal questionMoney) {
-		if (JdbcHelper.queryOne(AwAnswerRefundJob.class, "order_code", questionCode) == null) {
+	private void sentToRefund(AwQuestionInfo qtInfo) {
+		MDataMap wh = new MDataMap();
+		wh.put("product_code", qtInfo.getCode());
+		wh.put("seller_code", qtInfo.getAnswerUserCode());
+		wh.put("buyer_code", qtInfo.getQuestionUserCode());
+		OcOrderInfo orderInfo = JdbcHelper.queryOne(OcOrderInfo.class, "", "",
+				"code in (select code from oc_order_detail where product_code=:product_code) "
+						+ "and order_type='dzsd4112100110010003' and seller_code=:seller_code and buyer_code=:buyer_code",
+				wh);
+		if (JdbcHelper.queryOne(AwAnswerRefundJob.class, "order_code",
+				orderInfo != null && StringUtils.isNotBlank(orderInfo.getCode()) ? orderInfo.getCode()
+						: qtInfo.getCode()) == null) {
 			AwAnswerRefundJob job = new AwAnswerRefundJob();
-			job.setOrderCode(questionCode);
+			job.setOrderCode(orderInfo != null && StringUtils.isNotBlank(orderInfo.getCode()) ? orderInfo.getCode()
+					: qtInfo.getCode());
+			job.setQuestionCode(qtInfo.getCode());
 			job.setType("dzsd4888100110040002");// 拒绝回答
 			job.setCode(WebHelper.upCode("TK"));
-			job.setAmount(questionMoney);
+			job.setAmount(qtInfo.getMoney());
 			job.setAlAmount(BigDecimal.ZERO);
 			job.setRemark("达人拒绝回答，退款处理");
 			job.setStatus("0");
 			job.setUnAmount(BigDecimal.ZERO);
-			job.setUserCode(userCode);
-			UcUserinfoSocial us = JdbcHelper.queryOne(UcUserinfoSocial.class, "user_code", userCode, "account_type",
-					SocialEnum.wechat.name());
+			job.setUserCode(qtInfo.getQuestionUserCode());
+			UcUserinfoSocial us = JdbcHelper.queryOne(UcUserinfoSocial.class, "user_code", qtInfo.getQuestionUserCode(),
+					"account_type", SocialEnum.wechat.name());
 			if (us != null && StringUtils.isNotBlank(us.getAccountId())) {
 				job.setWechatOpenId(us.getAccountId());
 				JdbcHelper.insert(job);
