054a3e63f4d6e9675b7cb9bceab1ed6751394b95
逄小帅 <@DESKTOP-P9ASMJO>
2016-07-25 17:48:19 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerOrder.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/answer/ApiForAnswerOrder.java
@@ -12,8 +12,10 @@ import com.uhutu.dcom.order.z.entity.OcOrderInfo;
 import com.uhutu.sportcenter.z.api.ApiFactory;
 import com.uhutu.sportcenter.z.input.ApiForAnswerOrderInput;
 import com.uhutu.sportcenter.z.input.ApiWechatH5PayInput;
+import com.uhutu.sportcenter.z.input.ApiWechatMobilePayInput;
 import com.uhutu.sportcenter.z.result.ApiForAnswerOrderResult;
 import com.uhutu.sportcenter.z.result.ApiWechatH5PayResult;
+import com.uhutu.sportcenter.z.result.ApiWechatMobilePayResult;
 import com.uhutu.zoocom.root.RootApiToken;
 
 /**
@@ -45,20 +47,83 @@ public class ApiForAnswerOrder extends RootApiToken<ApiForAnswerOrderInput, ApiF
 			result.setStatus(reTeslaXResult.getStatus());
 			result.setError(reTeslaXResult.getError());
 		} else {
-			ApiWechatH5PayInput ai = new ApiWechatH5PayInput();
-			ai.setOrderCode(teslaXOrder.getOrderInfo().getCode());
-			ai.setRomoteIP(input.getRomoteIP());
-			ai.setServeIP(input.getServeIP());
-			ai.setPayMoney(teslaXOrder.getOrderInfo().getOrderMoney());
-			ai.setZoo(input.getZoo());
-			ApiWechatH5PayResult payResult = apiFactory.getApiWechatH5Pay().api(ai);
-			if(payResult.upFlagTrue()){
-				result.setWechatH5PayResponse(payResult.getWechatH5PayInfo());
-			}else {
-				result.setStatus(payResult.getStatus());
-				result.setError(payResult.getError());
-			}
+			
+			initPayResponse(teslaXOrder, result, input);
+			
 		}
 		return result;
+	}
+	
+	
+	/**
+	 * 初始化支付信息
+	 * @param teslaXOrder
+	 * @param result
+	 * @param input
+	 */
+	public void initPayResponse(TeslaXOrder teslaXOrder,ApiForAnswerOrderResult result,ApiForAnswerOrderInput input){
+		
+		switch (teslaXOrder.getOrderInfo().getOrderSource()) {
+		case "dzsd4112100110020001"://app订单
+			initMobilePayInfo(teslaXOrder, result, input);
+			break;
+		
+		case "dzsd4112100110020002"://h5订单
+			initH5PayInfo(teslaXOrder, result, input);
+			break;
+
+		default:
+			break;
+		}		
+		
+	}
+	
+	/**
+	 * 初始化移动支付信息
+	 * @param teslaXOrder
+	 * @param result
+	 * @param input
+	 */
+	public void initMobilePayInfo(TeslaXOrder teslaXOrder,ApiForAnswerOrderResult result,ApiForAnswerOrderInput input){
+		
+		ApiWechatMobilePayInput mobilePayInput = new ApiWechatMobilePayInput();
+		mobilePayInput.setOrderCode(teslaXOrder.getOrderInfo().getCode());
+		mobilePayInput.setRomoteIP(input.getRomoteIP());
+		mobilePayInput.setServeIP(input.getServeIP());
+		mobilePayInput.setPayMoney(teslaXOrder.getOrderInfo().getOrderMoney());
+		mobilePayInput.setZoo(input.getZoo());
+		ApiWechatMobilePayResult payResult = apiFactory.getApiWechatMobilePay().api(mobilePayInput);
+		if(payResult.upFlagTrue()){
+			result.setWechatMobilePayResponse(payResult.getMobilePayInfo());
+		}else {
+			result.setStatus(payResult.getStatus());
+			result.setError(payResult.getError());
+		}
+		
+	}
+	
+	/**
+	 * h5支付信息
+	 * @param teslaXOrder
+	 * @param result
+	 * @param input
+	 */
+	public void initH5PayInfo(TeslaXOrder teslaXOrder,ApiForAnswerOrderResult result,ApiForAnswerOrderInput input){
+		
+		ApiWechatH5PayInput ai = new ApiWechatH5PayInput();
+		ai.setOrderCode(teslaXOrder.getOrderInfo().getCode());
+		ai.setRomoteIP(input.getRomoteIP());
+		ai.setServeIP(input.getServeIP());
+		ai.setPayMoney(teslaXOrder.getOrderInfo().getOrderMoney());
+		ai.setZoo(input.getZoo());
+		ApiWechatH5PayResult payResult = apiFactory.getApiWechatH5Pay().api(ai);
+		if(payResult.upFlagTrue()){
+			result.setWechatH5PayResponse(payResult.getWechatH5PayInfo());
+		}else {
+			result.setStatus(payResult.getStatus());
+			result.setError(payResult.getError());
+		}
+		
+		
 	}
 }
