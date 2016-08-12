87d3161a7737de1a0ba05a98b48ff767f91069d1
xiegj <xiegj@ichsy.com>
2016-08-12 14:59:13 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiAttendList.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiAttendList.java
@@ -17,72 +17,72 @@ import com.uhutu.zoocom.root.RootApiBase;
 
 /**
  * 用户关注信息列表
+ * 
  * @author 逄小帅
  *
  */
 @Component
 public class ApiAttendList extends RootApiBase<ApiAttendListInput, ApiAttendListResult> {
-	
+
 	@Autowired
 	private UserServiceFactory userServiceFactory;
 
 	@Override
 	protected ApiAttendListResult process(ApiAttendListInput input) {
-		
+
 		ApiAttendListResult result = new ApiAttendListResult();
-		
+
 		QueryConditions conditions = new QueryConditions();
-		
+
 		conditions.setConditionEqual("attention", input.getUserCode());
-		
+
 		conditions.setConditionEqual("status", UserEnum.ATTEND.getCode());
-		
-		Page<UcAttentionInfo> attendPage = userServiceFactory.getAttentionInfoService().queryPageByCondition(input.getPagination(), 10, conditions);
-		
-		if(attendPage.hasNext()){
-			
+
+		Page<UcAttentionInfo> attendPage = userServiceFactory.getAttentionInfoService()
+				.queryPageByCondition(input.getPagination(), 10, conditions);
+
+		if (attendPage.hasNext()) {
+
 			result.setNextPageFlag(true);
-			
-		}else{
-			
+
+		} else {
+
 			result.setNextPageFlag(false);
-			
+
 		}
-		
-		if(attendPage.hasContent()){
-			
-			attendPage.getContent().forEach(attendInfo ->{
-				
+
+		if (attendPage.hasContent()) {
+
+			attendPage.getContent().forEach(attendInfo -> {
+
 				ApiAttendInfo apiAttendInfo = new ApiAttendInfo();
-				
+
 				apiAttendInfo.setUserCode(attendInfo.getBeAttention());
-				
-				UcUserinfoExt ucUserinfoExt = userServiceFactory.getUserInfoExtService().queryByUserCode(attendInfo.getBeAttention());
-				
-				UcUserinfo ucUserinfo = userServiceFactory.getUserInfoService().queryByCode(attendInfo.getBeAttention());
-				
-				if(ucUserinfoExt != null && ucUserinfo != null){
-					
+
+				UcUserinfoExt ucUserinfoExt = userServiceFactory.getUserInfoExtService()
+						.queryByUserCode(attendInfo.getBeAttention());
+
+				UcUserinfo ucUserinfo = userServiceFactory.getUserInfoService()
+						.queryByCode(attendInfo.getBeAttention());
+
+				if (ucUserinfoExt != null && ucUserinfo != null) {
+
 					apiAttendInfo.setAboutHead(ucUserinfoExt.getAboutHead());
-					
+
 					apiAttendInfo.setNickName(ucUserinfoExt.getNickName());
-					
+
 					apiAttendInfo.setType(ucUserinfo.getType());
-					
+
 					apiAttendInfo.setUserCode(ucUserinfo.getCode());
-					
+
 					result.getAttendList().add(apiAttendInfo);
-					
-				}else{
-					
-					result.inError(81100003);
-					
+
 				}
-				
+
 			});
-			
+
 		}
-		
+
 		return result;
 	}
 
