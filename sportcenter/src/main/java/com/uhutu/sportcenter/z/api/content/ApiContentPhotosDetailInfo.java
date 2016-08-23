4b541cb9f5de66bc4bec3ac004057cbd6e3885ab
xiegj <xiegj@ichsy.com>
2016-08-23 16:30:23 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiContentPhotosDetailInfo.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiContentPhotosDetailInfo.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Service;
 import com.uhutu.dcom.content.z.entity.CnContentBasicinfo;
 import com.uhutu.dcom.content.z.entity.CnContentPhotos;
 import com.uhutu.dcom.content.z.service.ContentServiceFactory;
+import com.uhutu.dcom.tag.z.service.ContentLabelServiceFactory;
 import com.uhutu.dcom.user.z.entity.UcUserinfo;
 import com.uhutu.dcom.user.z.entity.UcUserinfoExt;
 import com.uhutu.dcom.user.z.support.UserInfoSupport;
+import com.uhutu.sportcenter.z.api.util.ContentComponent;
 import com.uhutu.sportcenter.z.entity.ContentPhotosDetail;
 import com.uhutu.sportcenter.z.input.ApiContentPhotosInput;
 import com.uhutu.sportcenter.z.result.ApiContentPhotosResult;
@@ -32,82 +34,88 @@ public class ApiContentPhotosDetailInfo extends RootApiBase<ApiContentPhotosInpu
 
 	@Autowired
 	private ContentServiceFactory serviceFactory;
-	
+
 	@Autowired
 	private UserInfoSupport userInfoSupport;
-	
+
+	@Autowired
+	private ContentLabelServiceFactory labelServiceFacotry;
+
 	@Override
 	protected ApiContentPhotosResult process(ApiContentPhotosInput input) {
-		
+
 		ApiContentPhotosResult result = new ApiContentPhotosResult();
-		
-		CnContentBasicinfo cnContentBasicinfo = serviceFactory.getContentBasicinfoService().queryByCode(input.getContent_code());
-		
-		if(cnContentBasicinfo != null){
-			
+
+		CnContentBasicinfo cnContentBasicinfo = serviceFactory.getContentBasicinfoService()
+				.queryByCode(input.getContent_code());
+
+		if (cnContentBasicinfo != null) {
+
 			BeanUtils.copyProperties(cnContentBasicinfo, result.getContentBasicInfo());
-			
+
 			UcUserinfoExt ucUserinfoExt = userInfoSupport.getUserInfoExt(cnContentBasicinfo.getAuthor());
-			
+
 			UcUserinfo ucUserinfo = userInfoSupport.getUserInfo(cnContentBasicinfo.getAuthor());
-			
-			if(ucUserinfoExt != null){
-				
+
+			if (ucUserinfoExt != null) {
+
 				result.getContentBasicInfo().getUserBasicInfo().setNickName(ucUserinfoExt.getNickName());
-				
+
 				result.getContentBasicInfo().getUserBasicInfo().setAboutHead(ucUserinfoExt.getAboutHead());
-				
+
 			}
-			
-			if(ucUserinfo != null){
-				
+
+			if (ucUserinfo != null) {
+
 				result.getContentBasicInfo().getUserBasicInfo().setType(ucUserinfo.getType());
-				
+
 				result.getContentBasicInfo().getUserBasicInfo().setUserCode(ucUserinfo.getCode());
-				
+
 			}
-			
-			List<CnContentPhotos> cnContentPhotos = serviceFactory.getContentPhotosService().queryByContentCode(input.getContent_code());
-			
-			
+
+			List<CnContentPhotos> cnContentPhotos = serviceFactory.getContentPhotosService()
+					.queryByContentCode(input.getContent_code());
+
+			result.getContentBasicInfo()
+					.setTags(labelServiceFacotry.getContentLabelService().getLabels(cnContentBasicinfo.getTagCode()));
+			result.getContentBasicInfo().setFavorFlag(
+					ContentComponent.lightFavor(result.getContentBasicInfo().getCode(), input.getZoo().getToken()));
+
 			List<ContentPhotosDetail> cnContentPhotosDetails = new ArrayList<ContentPhotosDetail>();
-			
-			if(cnContentPhotos != null){
-				
+
+			if (cnContentPhotos != null) {
+
 				result.setTotalCount(cnContentPhotos.size());
-				
-				cnContentPhotos.forEach( entity -> {
-					
+
+				cnContentPhotos.forEach(entity -> {
+
 					ContentPhotosDetail contentPhotosDetail = new ContentPhotosDetail();
-					
+
 					BeanUtils.copyProperties(entity, contentPhotosDetail);
-					
-					if(StringUtils.isNotBlank(contentPhotosDetail.getPicture())){
-						
+
+					if (StringUtils.isNotBlank(contentPhotosDetail.getPicture())) {
+
 						ImageThumb thumb = ImageHelper.upThumbWithHeight(contentPhotosDetail.getPicture(), 0);
-						
+
 						contentPhotosDetail.setThumb(thumb);
-						
+
 					}
-					
+
 					cnContentPhotosDetails.add(contentPhotosDetail);
-					
-				} );
-				
+
+				});
+
 				result.setContentPhotosDetails(cnContentPhotosDetails);
-				
+
 			}
-			
-			
-			
-		}else{
-			
+
+		} else {
+
 			result.setStatus(0);
-			
+
 			result.setError("此内容信息不存在");
-			
+
 		}
-		
 
 		return result;
 	}
