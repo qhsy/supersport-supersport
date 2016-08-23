d729adbba1013c4ef0aafd388d2520514aa07908
xiegj <xiegj@ichsy.com>
2016-08-23 09:54:33 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/buttock/ApiButtockNotes.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/buttock/ApiButtockNotes.java
@@ -17,7 +17,7 @@ public class ApiButtockNotes extends RootApiBase<ApiButtockNotesInput, ApiButtoc
 
 	@Override
 	protected ApiButtockNotesResult process(ApiButtockNotesInput input) {
-		String str = "为翘而生美臀大赛海选Top30人气学员集结「实力派」，快为你最喜欢的选手点赞！最佳人气奖由你们来决定！\n【8月27号北京朝阳公园海沙节线下决赛，敬请关注！】";
+		String str = "为翘而生美臀大赛海选进入决赛的人气学员集结「实力派」，快为你喜欢的选手点赞！果冻美臀宝贝由你来决定！\n【8月27号北京朝阳公园海沙节线下决赛，敬请关注！】";
 		ApiButtockNotesResult result = new ApiButtockNotesResult();
 		result.setNotes(str);
 		return result;
