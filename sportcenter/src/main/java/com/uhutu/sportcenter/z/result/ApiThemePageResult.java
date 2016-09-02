98e3ef9858412f8f612f84a26865584a6ed9e884
xiegj <xiegj@ichsy.com>
2016-09-02 14:20:14 +0800

    推荐大人

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiThemePageResult.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiThemePageResult.java
@@ -3,7 +3,6 @@ package com.uhutu.sportcenter.z.result;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.uhutu.sportcenter.z.entity.RecommExpertInfo;
 import com.uhutu.sportcenter.z.entity.ThemePageModel;
 import com.uhutu.zoocom.root.RootApiResult;
 
@@ -29,16 +28,8 @@ public class ApiThemePageResult extends RootApiResult {
 	@ApiModelProperty(name = "详情信息")
 	List<ThemePageModel> models = new ArrayList<ThemePageModel>();
 
-	@ApiModelProperty(name = "推荐达人")
-	List<RecommExpertInfo> recomms = new ArrayList<RecommExpertInfo>();
-
-	public List<RecommExpertInfo> getRecomms() {
-		return recomms;
-	}
-
-	public void setRecomms(List<RecommExpertInfo> recomms) {
-		this.recomms = recomms;
-	}
+	@ApiModelProperty(name = "达人信息")
+	List<ThemePageModel> recomms = new ArrayList<ThemePageModel>();
 
 	public String getCode() {
 		return code;
@@ -86,6 +77,14 @@ public class ApiThemePageResult extends RootApiResult {
 
 	public void setShareFlag(boolean shareFlag) {
 		this.shareFlag = shareFlag;
+	}
+
+	public List<ThemePageModel> getRecomms() {
+		return recomms;
+	}
+
+	public void setRecomms(List<ThemePageModel> recomms) {
+		this.recomms = recomms;
 	}
 
 }
