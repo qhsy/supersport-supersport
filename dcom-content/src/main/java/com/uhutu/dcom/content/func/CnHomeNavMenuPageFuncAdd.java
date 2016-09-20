1cbbe7dc43f8de77a4fd5c61025b402d1f36c992
xiegj <xiegj@ichsy.com>
2016-09-20 15:14:19 +0800

    x

---
+++ dcom-content/src/main/java/com/uhutu/dcom/content/func/CnHomeNavMenuPageFuncAdd.java
@@ -0,0 +1,32 @@
+package com.uhutu.dcom.content.func;
+
+import com.uhutu.zoocom.helper.DateHelper;
+import com.uhutu.zoocom.model.MDataMap;
+import com.uhutu.zoodata.z.helper.JdbcHelper;
+import com.uhutu.zooweb.api.webpage.WebOperateInput;
+import com.uhutu.zooweb.api.webpage.WebOperateResult;
+import com.uhutu.zooweb.api.webpage.WebPageModel;
+import com.uhutu.zooweb.helper.WebHelper;
+import com.uhutu.zooweb.model.ExtendPageDefine;
+import com.uhutu.zooweb.root.RootFunc;
+
+public class CnHomeNavMenuPageFuncAdd extends RootFunc {
+
+	@Override
+	public WebOperateResult process(WebPageModel webPageModel, ExtendPageDefine extendPageDefine,
+			WebOperateInput input) {
+		WebOperateResult result = new WebOperateResult();
+		if (input.getDataMap().get("start_time").equals(input.getDataMap().get("end_time"))
+				|| DateHelper.parseDate(input.getDataMap().get("start_time"))
+						.after(DateHelper.parseDate(input.getDataMap().get("end_time")))) {
+			result.inError(810710004);
+		}
+		if (result.upFlagTrue()) {
+			MDataMap insert = input.getDataMap();
+			insert.put("code", WebHelper.upCode("DHLTBBH"));
+			JdbcHelper.dataInsert(extendPageDefine.getPageSource().getTableName(), input.getDataMap());
+		}
+		return result;
+	}
+
+}
