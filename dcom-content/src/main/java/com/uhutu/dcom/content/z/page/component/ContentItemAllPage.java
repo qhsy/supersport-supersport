7e3d51e7e6cf3fe223db7344ca7761268ad76ba4
xiegj <xiegj@ichsy.com>
2016-09-27 17:01:44 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/component/ContentItemAllPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/component/ContentItemAllPage.java
@@ -2,10 +2,12 @@ package com.uhutu.dcom.content.z.page.component;
 
 import com.uhutu.dcom.content.z.entity.CnContentItem;
 import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.define.DefineWebDeploy;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
-@ZooPage(name = "栏目管理", operates = DefineZooWeb.PAGE_DIALOG_OPERATE)
+@ZooPage(name = "栏目管理", operates = DefineZooWeb.PAGE_DIALOG_OPERATE, deploy = DefineWebDeploy.Grid_Where
+		+ "=type!='dzsd4107100110060003' ")
 public class ContentItemAllPage extends RootPageSimple<CnContentItem> {
 
 }
