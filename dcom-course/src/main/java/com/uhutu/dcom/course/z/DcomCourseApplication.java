b541a527d9878d5dd3ca608f79391d0f3a5aa891
upupboys <pangjinhui0522@126.com>
2016-10-13 16:18:48 +0800

    x

---
+++ dcom-course/src/main/java/com/uhutu/dcom/course/z/DcomCourseApplication.java
@@ -0,0 +1,12 @@
+package com.uhutu.dcom.course.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcomCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcomCourseApplication.class, args);
+	}
+}
