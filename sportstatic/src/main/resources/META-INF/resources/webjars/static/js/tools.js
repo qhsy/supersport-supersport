35a351ca4be93a7e2410dcdfb65d903a53958679
xiegj <xiegj@ichsy.com>
2016-10-27 16:07:17 +0800

    x

--- sportstatic/src/main/resources/META-INF/resources/webjars/static/js/tools.js
+++ sportstatic/src/main/resources/META-INF/resources/webjars/static/js/tools.js
@@ -135,6 +135,7 @@ function callbackPublish(type, code, name){
  * @author [wangxh]
  */
 function callbackSavePic(href){
+	console.log(1)
 	var userAgent = navigator.userAgent;
 	if(userAgent.indexOf('Android') != -1){
 		javascript:android.callbackSavePic(href);
