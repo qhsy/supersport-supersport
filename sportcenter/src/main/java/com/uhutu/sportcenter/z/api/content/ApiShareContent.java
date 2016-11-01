abe11e055f77ba12fc6f3b05c1bbb60739fc622b
xiegj <xiegj@ichsy.com>
2016-11-01 09:58:42 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiShareContent.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiShareContent.java
@@ -3,6 +3,7 @@ package com.uhutu.sportcenter.z.api.content;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import com.uhutu.dcom.content.z.entity.CnContentBasicinfo;
 import com.uhutu.dcom.content.z.entity.CnContentRecomm;
 import com.uhutu.dcom.content.z.service.ContentServiceFactory;
@@ -14,58 +15,47 @@ import com.uhutu.zooweb.helper.ImageHelper;
 
 /**
  * 分享内容
+ * 
  * @author 逄小帅
  *
  */
 @Component
 public class ApiShareContent extends RootApiBase<ApiShareContentInput, ApiShareContentResult> {
-	
+
 	@Autowired
 	private ContentServiceFactory contentServiceFactory;
 
 	@Override
 	protected ApiShareContentResult process(ApiShareContentInput input) {
-		
+
 		ApiShareContentResult shareResult = new ApiShareContentResult();
-		
-		CnContentBasicinfo basicInfo = contentServiceFactory.getContentBasicinfoService().queryByCode(input.getContentCode());
-		
-		CnContentRecomm recomm = contentServiceFactory.getContentRecommService().queryEntityByCode(input.getContentCode());
-		
-		if(basicInfo != null){
-			
+
+		CnContentBasicinfo basicInfo = contentServiceFactory.getContentBasicinfoService()
+				.queryByCode(input.getContentCode());
+
+		CnContentRecomm recomm = contentServiceFactory.getContentRecommService()
+				.queryEntityByCode(input.getContentCode());
+
+		if (basicInfo != null) {
+
 			shareResult.setTitle(basicInfo.getTitle());
-			
-			if(StringUtils.isNotEmpty(basicInfo.getCover())){
-				
+			if (StringUtils.isNotBlank(basicInfo.getCover())) {
 				shareResult.setIconUrl(ImageHelper.upImageThumbnail(basicInfo.getCover(), 120));
-				
-				if(recomm != null){
-					
-					if(StringUtils.isNotBlank(recomm.getContent())){
-						
-						shareResult.setContent(recomm.getContent());
-						
-					}else{
-						
-						shareResult.setContent(TopHelper.upInfo(810710011));
-						
-					}
-					
-				}else{
-					
-					shareResult.setContent(TopHelper.upInfo(810710011));
-					
-				}
-				
 			}
-			
-		}else{
-			
+			if (StringUtils.isNotBlank(basicInfo.getTagCode()) && (basicInfo.getTagCode().contains("GGBH161020110001")
+					|| basicInfo.getTagCode().contains("GGBH161020210001")
+					|| basicInfo.getTagCode().contains("GGBH161020210002")
+					|| basicInfo.getTagCode().contains("GGBH161020210003"))) {
+				shareResult.setContent(TopHelper.upInfo(810710021));
+			} else if (recomm != null && StringUtils.isNotBlank(recomm.getContent())) {
+				shareResult.setContent(recomm.getContent());
+			} else {
+				shareResult.setContent(TopHelper.upInfo(810710011));
+			}
+		} else {
 			shareResult.inError(810710008);
-			
 		}
-		
+
 		return shareResult;
 	}
 
