7251d6df322d9f8302505474a42f20ee5f604ccd
upupboys <pangjinhui0522@126.com>
2016-11-03 17:29:42 +0800

    增加搜索

---
+++ dcom-search/src/main/java/com/uhutu/dcom/search/DcomSearchApplication.java
@@ -0,0 +1,12 @@
+package com.uhutu.dcom.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcomSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcomSearchApplication.class, args);
+	}
+}
