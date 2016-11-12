4619c928c832569907ab1ffef1719576b5a51efd
upupboys <pangjinhui0522@126.com>
2016-11-12 17:09:44 +0800

    x

---
+++ dcom-component/src/main/java/com/uhutu/dcom/component/z/hole/ImageCfUtil.java
@@ -0,0 +1,177 @@
+package com.uhutu.dcom.component.z.hole;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import com.uhutu.dcom.component.z.util.ImageUtil;
+import com.uhutu.zoocom.file.FileUploadResult;
+import com.uhutu.zooweb.support.WebUploadSupport;
+
+/**
+ * crossfit个人证书生成
+ * @author 逄小帅
+ *
+ */
+public class ImageCfUtil {
+
+	/**
+	 * 合成个人图片
+	 * @param name
+	 * 		用户名称
+	 * @param box
+	 * 		所属box
+	 * @param typeName
+	 * 		参赛类型名称
+	 * @param code
+	 * 		参赛码
+	 * @return 文件上传结果
+	 */
+	public static FileUploadResult makeImagePerson(String name,String box,String typeName,String code) {	
+		
+		FileUploadResult webUploadResult = new FileUploadResult();
+		
+		try {
+			
+			String url = ClassLoader.getSystemResource("hole/crossfit").getPath();
+			
+			/*背景图*/
+			Image srcImg = ImageIO.read(new File(url+"/person/bg.png"));
+			BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null), srcImg.getHeight(null),
+					BufferedImage.TYPE_INT_RGB);
+			// 得到画笔对象
+			// Graphics g= buffImg.getGraphics();
+			Graphics2D g = buffImg.createGraphics();
+
+			// 设置对线段的锯齿状边缘处理
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+			g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0,
+					0, null);
+			/*设置旋转*/
+			g.rotate(0);
+			
+			float alpha = 1.0f;
+			
+			/*活动icon*/
+			ImageUtil.drawImageCf("/person/corplogo.png", g, alpha, 202, 12, 102, 100);
+			
+			/*果冻体育icon*/
+			ImageUtil.drawImageCf("/person/gdlogo.png", g, alpha, 320, 40, 204, 48);
+			
+			/*头像icon*/
+			ImageUtil.drawImageCf("/person/head.jpeg", g, alpha, 196, 150, 364, 364);
+			
+			/*throwdown 水印*/
+			ImageUtil.drawImageCf("/person/throwdown.png", g, alpha, 52, 120, 308, 264);			
+	        
+			/*姓名*/
+	        ImageUtil.drawStrCf(name, g, 0, 48, 306, 576);
+	        /*所属box*/
+	        ImageUtil.drawStrCf(box, g, 0, 32, 286, 638);
+	        /*参赛码*/
+	        ImageUtil.drawStrCf(typeName+"参赛码："+code, g, 0, 36, 162, 690);
+	        /*宣传语*/
+	        ImageUtil.drawImageCf("/person/kill.png", g, alpha, 70, 758, 594, 66);
+	        /*group icon*/
+	        ImageUtil.drawImageCf("/person/group.png", g, alpha, 50, 838, 424, 250);
+			
+			g.dispose();
+			
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			// 生成图片
+			ImageIO.write(buffImg, "PNG", bos);
+			
+			webUploadResult = new WebUploadSupport().remoteUpload("crossfit", "person.png", bos.toByteArray());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return webUploadResult;
+		
+	}   
+	
+	/**
+	 * 合成团队图片
+	 * @param name
+	 * 		用户名称
+	 * @param box
+	 * 		所属box
+	 * @param typeName
+	 * 		参赛类型名称
+	 * @param code
+	 * 		参赛码
+	 * @return 文件上传结果
+	 */
+	public static FileUploadResult makeImageGroup(String name,String box,String typeName,String code) {	
+		
+		FileUploadResult webUploadResult = new FileUploadResult();
+		
+		try {
+			
+			String url = ClassLoader.getSystemResource("hole/crossfit").getPath();
+			
+			/*背景图*/
+			Image srcImg = ImageIO.read(new File(url+"/person/bg.png"));
+			BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null), srcImg.getHeight(null),
+					BufferedImage.TYPE_INT_RGB);
+			// 得到画笔对象
+			// Graphics g= buffImg.getGraphics();
+			Graphics2D g = buffImg.createGraphics();
+
+			// 设置对线段的锯齿状边缘处理
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+			g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0,
+					0, null);
+			/*设置旋转*/
+			g.rotate(0);
+			
+			float alpha = 1.0f;
+			
+			/*活动icon*/
+			ImageUtil.drawImageCf("/person/corplogo.png", g, alpha, 202, 12, 102, 100);
+			
+			/*果冻体育icon*/
+			ImageUtil.drawImageCf("/person/gdlogo.png", g, alpha, 320, 40, 204, 48);
+			
+			/*头像icon*/
+			ImageUtil.drawImageCf("/person/head.jpeg", g, alpha, 196, 150, 364, 364);
+			
+			/*throwdown 水印*/
+			ImageUtil.drawImageCf("/person/throwdown.png", g, alpha, 52, 120, 308, 264);			
+	        
+			/*姓名*/
+	        ImageUtil.drawStrCf(name, g, 0, 48, 306, 576);
+	        /*所属box*/
+	        ImageUtil.drawStrCf(box, g, 0, 32, 286, 638);
+	        /*参赛码*/
+	        ImageUtil.drawStrCf(typeName+"参赛码："+code, g, 0, 36, 162, 690);
+	        /*宣传语*/
+	        ImageUtil.drawImageCf("/person/kill.png", g, alpha, 70, 758, 594, 66);
+	        /*group icon*/
+	        ImageUtil.drawImageCf("/person/group.png", g, alpha, 50, 838, 424, 250);
+			
+			g.dispose();
+			
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			// 生成图片
+			ImageIO.write(buffImg, "PNG", bos);
+			
+			webUploadResult = new WebUploadSupport().remoteUpload("crossfit", "group.png", bos.toByteArray());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return webUploadResult;
+		
+	}   
+
+}
---
+++ dcom-component/src/main/java/com/uhutu/dcom/component/z/util/ImageUtil.java
@@ -0,0 +1,61 @@
+package com.uhutu.dcom.component.z.util;
+
+import java.awt.AlphaComposite;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.font.TextAttribute;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedString;
+
+import javax.swing.ImageIcon;
+
+/**
+ * 图片处理工具类
+ * @author 逄小帅
+ *
+ */
+public class ImageUtil {
+	
+	
+	
+	
+	public static void drawStrCf(String content,Graphics2D g,int fontStyle,int fontSize,int x,int y){		
+		
+		AttributedString ats = new AttributedString(content);
+		
+	    Font f = new Font("微软雅黑",fontStyle, fontSize);
+
+	    ats.addAttribute(TextAttribute.FONT, f, 0,content.length() );
+	    
+	    AttributedCharacterIterator iter = ats.getIterator();
+	    
+	    g.drawString(iter, x, y);
+		
+	}
+	
+	/**
+	 * 合成图片
+	 * @param abPath
+	 * 		相对路径
+	 * @param g
+	 * 		画笔
+	 * @param alpha
+	 * 		透明度
+	 */
+	public static void drawImageCf(String abPath,Graphics2D g,float alpha,int x,int y,int width,int height){
+		
+		String realPath = ClassLoader.getSystemResource("hole/crossfit").getPath();
+		
+		ImageIcon imageIcon2 = new ImageIcon(realPath + abPath);
+		
+		// 得到Image对象。
+		Image img2 = imageIcon2.getImage();
+		
+		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+		// 表示水印图片的位置
+		g.drawImage(img2, x, y, width, height, null);
+		
+	}
+	
+}
