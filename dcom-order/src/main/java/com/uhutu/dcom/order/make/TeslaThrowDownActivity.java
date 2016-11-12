98ad2e6581b09c12e25505a1de4414365b8ea1d7
xiegj <xiegj@ichsy.com>
2016-11-12 10:12:33 +0800

    x

--- dcom-order/src/main/java/com/uhutu/dcom/order/make/TeslaThrowDownActivity.java
+++ dcom-order/src/main/java/com/uhutu/dcom/order/make/TeslaThrowDownActivity.java
@@ -6,8 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.uhutu.dcom.component.z.util.ApplicationSupport;
 import com.uhutu.dcom.component.z.util.WebClientComponent;
 import com.uhutu.dcom.order.orderResult.TeslaXOrder;
 import com.uhutu.dcom.order.orderResult.TeslaXResult;
@@ -28,11 +29,8 @@ import com.uhutu.zooweb.helper.WebHelper;
  * @author xiegj
  *
  */
-// @Component
+@Service
 public class TeslaThrowDownActivity extends TeslaTopOrderMake {
-
-	@Autowired
-	private PayGateProcess payGateProcess;
 
 	public final static String PER_PRO = "dzsd4107100510020001";// 个人标准
 	public final static String PER_SPA = "dzsd4107100510020002";// 个人业余
@@ -46,8 +44,7 @@ public class TeslaThrowDownActivity extends TeslaTopOrderMake {
 
 				String type = teslaOrder.getSigns().get(0).getType();
 				String productName = type.equals(PER_PRO) ? "个人标准组（Rx）"
-						: (type.equals(PER_SPA) ? "个人业余组（Scale）"
-								: (type.equals(GRO_PRO) ? "团队标准组（Rx）" : ""));// dzsd4107100510020001:个人标准,dzsd4107100510020002:个人业余,dzsd4107100510020003:团体标准
+						: (type.equals(PER_SPA) ? "个人业余组（Scale）" : (type.equals(GRO_PRO) ? "团队标准组（Rx）" : ""));// dzsd4107100510020001:个人标准,dzsd4107100510020002:个人业余,dzsd4107100510020003:团体标准
 				teslaOrder.getDetail().get(0).setProductName(productName);
 				teslaOrder.getDetail().get(0).setProductPrice(getPrice().get(type));
 				teslaOrder.getOrderInfo().setSellerCode("系统");
@@ -109,8 +106,8 @@ public class TeslaThrowDownActivity extends TeslaTopOrderMake {
 					UcSignInfo signInfo = infos.get(j);
 					signInfo.setStatus("dzsd4112100110030001");
 					signInfo.setActivityName("2016 Beijing CrossFit ThrowDown");
-					WechatAccessTokenResponse tokenResponse = (WechatAccessTokenResponse) payGateProcess
-							.process(PayProcessEnum.WECHAT_TOKEN, null, new MDataMap());
+					WechatAccessTokenResponse tokenResponse = (WechatAccessTokenResponse) ((PayGateProcess) ApplicationSupport
+							.getBean("payGateProcess")).process(PayProcessEnum.WECHAT_TOKEN, null, new MDataMap());
 					signInfo.setPhoto(WebClientComponent
 							.wechatMediaDownLoad("crossfit", tokenResponse.getAccess_token(), signInfo.getPhoto())
 							.getFileUrl());
