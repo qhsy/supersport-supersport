1323a432ad8e384b432cb0a6458a5389732d8a0a
upupboys <pangjinhui0522@126.com>
2016-11-14 02:38:23 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/extend/ApiSignPhoto.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/extend/ApiSignPhoto.java
@@ -1,5 +1,8 @@
 package com.uhutu.sportcenter.z.api.extend;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -21,21 +24,63 @@ public class ApiSignPhoto extends RootApiToken<ApiSignPhotoInput, ApiSignPhotoRe
 
 	protected ApiSignPhotoResult process(ApiSignPhotoInput input) {
 		ApiSignPhotoResult result = new ApiSignPhotoResult();
+		
+		List<UcSignInfo> signInfos = new ArrayList<UcSignInfo>();
+		
 		List<UcSignInfo> li = JdbcHelper.queryForList(UcSignInfo.class, "", "",
 				" status=:status and userCode=:userCode and type in ('dzsd4107100510020001','dzsd4107100510020002')",
 				MapHelper.initMap("userCode", upUserCode(), "status", "dzsd4112100110030002"));
+		
 		if (li != null && li.size() > 0) {
 			for (int i = 0; i < li.size(); i++) {
-				result.getLi().add(li.get(i).getPicUrl());// 已报名
+				signInfos.add(li.get(i));// 已报名
 			}
 		}
 		List<UcSignInfo> li2 = JdbcHelper.queryForList(UcSignInfo.class, "", "",
 				" status=:status and userCode=:userCode and type ='dzsd4107100510020003'",
 				MapHelper.initMap("userCode", upUserCode(), "status", "dzsd4112100110030002"));
 		if (li2 != null && li2.size() > 0) {
-			result.getLi().add(li2.get(0).getPicUrl());// 已报名
+			signInfos.add(li2.get(0));// 已报名
 		}
+		
+		sort(signInfos);
+		
+		for (UcSignInfo ucSignInfo : signInfos) {
+			
+			if(ucSignInfo != null){
+				
+				result.getLi().add(ucSignInfo.getPicUrl());
+				
+			}
+			
+		}
+		
 		return result;
+	}
+	
+	public void sort(List<UcSignInfo> infos){
+		
+		if(infos != null){
+			
+			Collections.sort(infos, new Comparator<UcSignInfo>() {
+
+				@Override
+				public int compare(UcSignInfo o1, UcSignInfo o2) {
+
+					if (o1 != null && o2 != null) {
+
+						int result = o1.getZc().compareTo(o2.getZc());
+
+						return 0 - result;
+
+					}
+
+					return 0;
+				}
+			});
+			
+		}
+		
 	}
 
 }
