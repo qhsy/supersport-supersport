b6e6ff1e85d1a784a1c12b5cfa9c59a69ddca9c5
xiegj <xiegj@ichsy.com>
2016-11-14 01:41:38 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/extend/ApiSignPhoto.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/extend/ApiSignPhoto.java
@@ -22,14 +22,21 @@ public class ApiSignPhoto extends RootApiToken<ApiSignPhotoInput, ApiSignPhotoRe
 	protected ApiSignPhotoResult process(ApiSignPhotoInput input) {
 		ApiSignPhotoResult result = new ApiSignPhotoResult();
 		List<UcSignInfo> li = JdbcHelper.queryForList(UcSignInfo.class, "", "",
-				" status=:status and userCode=:userCode ",
+				" status=:status and userCode=:userCode and type in ('dzsd4107100510020001','dzsd4107100510020002')",
 				MapHelper.initMap("userCode", upUserCode(), "status", "dzsd4112100110030002"));
 		if (li != null && li.size() > 0) {
 			for (int i = 0; i < li.size(); i++) {
 				result.getLi().add(li.get(i).getPicUrl());// 已报名
 			}
 		}
-
+		List<UcSignInfo> li2 = JdbcHelper.queryForList(UcSignInfo.class, "", "",
+				" status=:status and userCode=:userCode and type ='dzsd4107100510020003'",
+				MapHelper.initMap("userCode", upUserCode(), "status", "dzsd4112100110030002"));
+		if (li2 != null && li2.size() > 0) {
+			for (int i = 0; i < li2.size(); i++) {
+				result.getLi().add(li2.get(i).getPicUrl());// 已报名
+			}
+		}
 		return result;
 	}
 
