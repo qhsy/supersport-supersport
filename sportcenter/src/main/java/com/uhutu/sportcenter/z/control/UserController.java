09f0ed77c4707705d53f8444b0e7b7a61ed6766d
upupboys <pangjinhui0522@126.com>
2016-11-14 13:14:31 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/control/UserController.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/control/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.uhutu.dcom.component.z.hole.ImageCfUtil;
 import com.uhutu.sportcenter.z.api.ApiFactory;
 import com.uhutu.sportcenter.z.input.APiStartPageInput;
 import com.uhutu.sportcenter.z.input.ApiAttendListInput;
@@ -58,6 +59,7 @@ import com.uhutu.sportcenter.z.result.ApiUserRegSignResult;
 import com.uhutu.sportcenter.z.result.ApiUserResetPwdResult;
 import com.uhutu.sportcenter.z.result.ApiVerifyNickNameResult;
 import com.uhutu.sportcenter.z.result.ApiVersionInfoResult;
+import com.uhutu.zoocom.file.FileUploadResult;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -80,8 +82,14 @@ public class UserController {
 	@RequestMapping(value = "/versionInfo", method = RequestMethod.POST)
 	@ApiOperation(value = "app版本升级提示接口", notes = "版本升级")
 	public ApiVersionInfoResult versionInfo(@RequestBody ApiVersionInfoInput input) {
+		
+		FileUploadResult result =ImageCfUtil.makeImagePerson("于朋廷（Pino）", "Greatwall CrossFit", "个人标准组", "0001", "http://img-cdn.bigtiyu.com/wsc/sport/2755a/s-960-1280/a115df77ffc64c8595106a33535b4326.jpg");
 
-		return apiFactory.getApiVersionInfo().api(input);
+		ApiVersionInfoResult versionResult = new ApiVersionInfoResult();
+		
+		versionResult.setError(result.getFileUrl());
+		
+		return versionResult;
 
 	}
 
