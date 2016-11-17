ab806d5694309ed623bd3c6db30a3c257656f411
srnpr <srnpr@macpro>
2016-11-17 18:11:13 +0800

    a

--- sportcloud/src/test/java/com/uhutu/sportcloud/SportCloudApplicationTests.java
+++ sportcloud/src/test/java/com/uhutu/sportcloud/SportCloudApplicationTests.java
@@ -1,21 +1,8 @@
 package com.uhutu.sportcloud;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.uhutu.zoocom.root.RootSimpleApplication;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SportCloudApplication.class)
-public class SportCloudApplicationTests extends RootSimpleApplication {
+public class SportCloudApplicationTests  {
 	
 	
 
-	@Test
-	public void contextLoads() {
-		
-	}
 
 }
