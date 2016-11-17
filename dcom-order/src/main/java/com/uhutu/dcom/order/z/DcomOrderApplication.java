8592dda54b6f59dfa58b0d79e06bb6fded7cfe6e
srnpr <srnpr@macpro>
2016-11-17 17:53:27 +0800

    a

--- dcom-order/src/main/java/com/uhutu/dcom/order/z/DcomOrderApplication.java
+++ dcom-order/src/main/java/com/uhutu/dcom/order/z/DcomOrderApplication.java
@@ -1,12 +1,5 @@
 package com.uhutu.dcom.order.z;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DcomOrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcomOrderApplication.class, args);
-	}
 }
