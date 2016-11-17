69290de2b3a24487028c5233d723db12a322d907
srnpr <srnpr@macpro>
2016-11-17 17:54:13 +0800

    a

--- dcom-remark/src/main/java/com/uhutu/dcom/remark/DcomRemarkApplication.java
+++ dcom-remark/src/main/java/com/uhutu/dcom/remark/DcomRemarkApplication.java
@@ -1,12 +1,6 @@
 package com.uhutu.dcom.remark;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DcomRemarkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcomRemarkApplication.class, args);
-	}
+	
 }
