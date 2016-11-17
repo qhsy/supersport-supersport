a4be413251ad8036d43eb48b2aee501a806197ab
srnpr <srnpr@macpro>
2016-11-17 14:57:30 +0800

    a

--- dcom-config/src/main/java/com/uhutu/dcom/config/DcomConfigApplication.java
+++ dcom-config/src/main/java/com/uhutu/dcom/config/DcomConfigApplication.java
@@ -1,12 +1,10 @@
 package com.uhutu.dcom.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 public class DcomConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DcomConfigApplication.class, args);
+		
 	}
 }
