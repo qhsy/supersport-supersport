4409d5528901471c8b18f3cadd57a654e3afa5d1
srnpr <srnpr@macpro>
2016-11-17 17:53:07 +0800

    a

--- dcom-evaluate/src/main/java/com/uhutu/dcom/evaluate/z/DcomEvaluateApplication.java
+++ dcom-evaluate/src/main/java/com/uhutu/dcom/evaluate/z/DcomEvaluateApplication.java
@@ -1,12 +1,6 @@
 package com.uhutu.dcom.evaluate.z;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DcomEvaluateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcomEvaluateApplication.class, args);
-	}
+	
 }
