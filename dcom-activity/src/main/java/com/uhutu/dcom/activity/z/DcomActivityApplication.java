4ec1d47f7b038bb4cec818b9fa8554a15964dc70
srnpr <srnpr@macpro>
2016-11-17 17:51:30 +0800

    a

--- dcom-activity/src/main/java/com/uhutu/dcom/activity/z/DcomActivityApplication.java
+++ dcom-activity/src/main/java/com/uhutu/dcom/activity/z/DcomActivityApplication.java
@@ -1,12 +1,6 @@
 package com.uhutu.dcom.activity.z;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DcomActivityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcomActivityApplication.class, args);
-	}
+	
 }
