55c0db43d0e27e8f22d3d4ea27830646cd25cd39
srnpr <srnpr@macpro>
2016-11-17 17:51:55 +0800

    a

--- dcom-answer/src/main/java/com/uhutu/dcom/answer/z/DcomAnswerApplication.java
+++ dcom-answer/src/main/java/com/uhutu/dcom/answer/z/DcomAnswerApplication.java
@@ -1,12 +1,5 @@
 package com.uhutu.dcom.answer.z;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DcomAnswerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcomAnswerApplication.class, args);
-	}
 }
