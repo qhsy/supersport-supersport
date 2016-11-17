b99543c7b5539a1b80e82ebc65be855d37c237a0
srnpr <srnpr@macpro>
2016-11-17 15:23:05 +0800

    a

--- dcom-order/src/test/java/com/uhutu/dcom/pay/DcomOrderApplicationTest.java
+++ dcom-order/src/test/java/com/uhutu/dcom/pay/DcomOrderApplicationTest.java
@@ -1,17 +1,8 @@
 package com.uhutu.dcom.pay;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.uhutu.dcom.order.z.DcomOrderApplication;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DcomOrderApplication.class)
 public class DcomOrderApplicationTest {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
