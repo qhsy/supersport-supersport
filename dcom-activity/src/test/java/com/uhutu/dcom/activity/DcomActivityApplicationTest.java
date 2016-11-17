f4b399f8e10f23cb5e2626d7703e412947ae9ea0
srnpr <srnpr@macpro>
2016-11-17 15:21:28 +0800

    a

--- dcom-activity/src/test/java/com/uhutu/dcom/activity/DcomActivityApplicationTest.java
+++ dcom-activity/src/test/java/com/uhutu/dcom/activity/DcomActivityApplicationTest.java
@@ -1,17 +1,8 @@
 package com.uhutu.dcom.activity;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.uhutu.dcom.activity.z.DcomActivityApplication;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DcomActivityApplication.class)
 public class DcomActivityApplicationTest {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
