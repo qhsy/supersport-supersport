e903368b625fd07d70433ffa255279e1d8fbe733
srnpr <srnpr@macpro>
2016-11-17 15:17:50 +0800

    a

--- sportcenter/src/test/java/com/uhutu/sportcenter/SportCenterApplicationTests.java
+++ sportcenter/src/test/java/com/uhutu/sportcenter/SportCenterApplicationTests.java
@@ -1,12 +1,5 @@
 package com.uhutu.sportcenter;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.uhutu.sportcenter.SportCenterApplication;
-
 public class SportCenterApplicationTests {
 
 	
