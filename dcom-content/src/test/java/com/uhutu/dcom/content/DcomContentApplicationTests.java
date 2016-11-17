186e2f4bd316a025bac11e9a8f5e8de5cddb50a7
srnpr <srnpr@macpro>
2016-11-17 14:56:53 +0800

    a

--- dcom-content/src/test/java/com/uhutu/dcom/content/DcomContentApplicationTests.java
+++ dcom-content/src/test/java/com/uhutu/dcom/content/DcomContentApplicationTests.java
@@ -7,11 +7,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.uhutu.dcom.content.z.DcomContentApplication;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DcomContentApplication.class)
 public class DcomContentApplicationTests {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
