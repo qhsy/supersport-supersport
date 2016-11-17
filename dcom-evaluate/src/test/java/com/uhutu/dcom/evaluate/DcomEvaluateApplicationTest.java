c7891854b435870ac43f838631916a70c364e824
srnpr <srnpr@macpro>
2016-11-17 15:22:43 +0800

    a

--- dcom-evaluate/src/test/java/com/uhutu/dcom/evaluate/DcomEvaluateApplicationTest.java
+++ dcom-evaluate/src/test/java/com/uhutu/dcom/evaluate/DcomEvaluateApplicationTest.java
@@ -1,17 +1,8 @@
 package com.uhutu.dcom.evaluate;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.uhutu.dcom.evaluate.z.DcomEvaluateApplication;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DcomEvaluateApplication.class)
 public class DcomEvaluateApplicationTest {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
