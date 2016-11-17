085b4b7e789a228b5b0d123b91fef440a28a9dae
srnpr <srnpr@macpro>
2016-11-17 17:52:48 +0800

    a

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/DcomContentApplication.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/DcomContentApplication.java
@@ -1,12 +1,6 @@
 package com.uhutu.dcom.content.z;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DcomContentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcomContentApplication.class, args);
-	}
+	
 }
