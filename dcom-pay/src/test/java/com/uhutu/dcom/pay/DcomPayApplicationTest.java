cdfa8833ead1f47462afea40823c41f37caf18e5
srnpr <srnpr@macpro>
2016-11-17 15:20:05 +0800

    a

--- dcom-pay/src/test/java/com/uhutu/dcom/pay/DcomPayApplicationTest.java
+++ dcom-pay/src/test/java/com/uhutu/dcom/pay/DcomPayApplicationTest.java
@@ -1,17 +1,10 @@
 package com.uhutu.dcom.pay;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.uhutu.dcom.pay.z.DcomPayApplication;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DcomPayApplication.class)
 public class DcomPayApplicationTest {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
