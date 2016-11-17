95edfa5c441323cc8a32db24122de855422f79d4
srnpr <srnpr@macpro>
2016-11-17 17:54:33 +0800

    a

--- dcom-tag/src/main/java/com/uhutu/dcom/tag/z/DcomTagApplication.java
+++ dcom-tag/src/main/java/com/uhutu/dcom/tag/z/DcomTagApplication.java
@@ -1,12 +1,5 @@
 package com.uhutu.dcom.tag.z;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DcomTagApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcomTagApplication.class, args);
-	}
 }
