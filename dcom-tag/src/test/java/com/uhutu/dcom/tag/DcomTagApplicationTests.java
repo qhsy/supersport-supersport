c1b4cad14818a5d598eaa1f2c40087db7f604716
srnpr <srnpr@macpro>
2016-11-17 15:23:51 +0800

    a

--- dcom-tag/src/test/java/com/uhutu/dcom/tag/DcomTagApplicationTests.java
+++ dcom-tag/src/test/java/com/uhutu/dcom/tag/DcomTagApplicationTests.java
@@ -1,17 +1,8 @@
 package com.uhutu.dcom.tag;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.uhutu.dcom.tag.z.DcomTagApplication;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DcomTagApplication.class)
 public class DcomTagApplicationTests {
 
-	@Test
+	
 	public void contextLoads() {
 	}
 
