443c282894fe5ca14b7e57df1b530dac4806ca43
xiegj <xiegj@ichsy.com>
2016-12-07 11:08:21 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/live/ApiLiveInfo.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/live/ApiLiveInfo.java
@@ -7,6 +7,8 @@ import com.uhutu.dcom.content.z.entity.CnContentBasicinfo;
 import com.uhutu.dcom.content.z.entity.CnLiveVideoDetail;
 import com.uhutu.dcom.content.z.properties.ConfigDcomContent;
 import com.uhutu.dcom.content.z.properties.SettingsDcomContent;
+import com.uhutu.dcom.user.z.entity.UcUserinfo;
+import com.uhutu.dcom.user.z.entity.UcUserinfoExt;
 import com.uhutu.sportcenter.z.input.ApiLiveInfoInput;
 import com.uhutu.sportcenter.z.result.ApiLiveInfoResult;
 import com.uhutu.zoocom.helper.MapHelper;
@@ -39,6 +41,15 @@ public class ApiLiveInfo extends RootApiBase<ApiLiveInfoInput, ApiLiveInfoResult
 				if (detail != null) {
 					SettingsDcomContent dcomContent = ConfigDcomContent.upConfig();
 					result.setAppId(dcomContent.getLiveAppId());
+				}
+				UcUserinfoExt ext = JdbcHelper.queryOne(UcUserinfoExt.class, "user_code", basicinfo.getAuthor());
+				UcUserinfo info = JdbcHelper.queryOne(UcUserinfo.class, "code", basicinfo.getAuthor());
+				if (ext != null && info != null) {
+					result.getUserBasicInfo().setAboutHead(ext.getAboutHead());
+					result.getUserBasicInfo().setNickName(ext.getNickName());
+					result.getUserBasicInfo().setTitle(ext.getTitle());
+					result.getUserBasicInfo().setType(info.getType());
+					result.getUserBasicInfo().setUserCode(ext.getUserCode());
 				}
 			}
 
--- sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiLiveInfoResult.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiLiveInfoResult.java
@@ -1,6 +1,7 @@
 package com.uhutu.sportcenter.z.result;
 
 import com.uhutu.dcom.content.z.entity.CnLiveVideoDetail;
+import com.uhutu.sportcenter.z.entity.UserBasicInfo;
 import com.uhutu.zoocom.root.RootApiResult;
 
 import io.swagger.annotations.ApiModelProperty;
@@ -12,6 +13,9 @@ public class ApiLiveInfoResult extends RootApiResult {
 	@ApiModelProperty(value = "直播appId", notes = "直播appId")
 	private String appId;
 
+	@ApiModelProperty(value = "主播基本信息")
+	private UserBasicInfo userBasicInfo = new UserBasicInfo();
+	
 	public CnLiveVideoDetail getDetail() {
 		return detail;
 	}
@@ -26,6 +30,14 @@ public class ApiLiveInfoResult extends RootApiResult {
 
 	public void setAppId(String appId) {
 		this.appId = appId;
+	}
+
+	public UserBasicInfo getUserBasicInfo() {
+		return userBasicInfo;
+	}
+
+	public void setUserBasicInfo(UserBasicInfo userBasicInfo) {
+		this.userBasicInfo = userBasicInfo;
 	}
 
 }
