a3f81579a2b95810653e600dc0555dbb0167ca1b
xiegj <xiegj@ichsy.com>
2016-12-07 18:46:03 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiReportFieldsResult.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/result/ApiReportFieldsResult.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.uhutu.dcom.extend.z.entity.ReReportInfo;
 import com.uhutu.sportcenter.z.entity.ReReportFieldForApi;
 import com.uhutu.zoocom.root.RootApiResult;
+
 import io.swagger.annotations.ApiModelProperty;
 
 public class ApiReportFieldsResult extends RootApiResult {
@@ -15,6 +16,9 @@ public class ApiReportFieldsResult extends RootApiResult {
 
 	@ApiModelProperty(name = "报表字段参数信息")
 	List<ReReportFieldForApi> fields = new ArrayList<ReReportFieldForApi>();
+
+	@ApiModelProperty(name = "按钮信息")
+	List<ReReportFieldForApi> buttons = new ArrayList<ReReportFieldForApi>();
 
 	public ReReportInfo getFs() {
 		return fs;
@@ -30,6 +34,14 @@ public class ApiReportFieldsResult extends RootApiResult {
 
 	public void setFields(List<ReReportFieldForApi> fields) {
 		this.fields = fields;
+	}
+
+	public List<ReReportFieldForApi> getButtons() {
+		return buttons;
+	}
+
+	public void setButtons(List<ReReportFieldForApi> buttons) {
+		this.buttons = buttons;
 	}
 
 }
