7c72cd064425242f8c8808f33268d1dffc264f29
upupboys <pangjinhui0522@126.com>
2016-12-13 17:48:10 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/support/AccountComponet.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/support/AccountComponet.java
@@ -1,6 +1,12 @@
 package com.uhutu.dcom.user.z.support;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.uhutu.dcom.user.z.entity.UcAccountInfo;
+import com.uhutu.dcom.user.z.service.IAccountInfoService;
 
 /**
  * 账户操作support
@@ -10,6 +16,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class AccountComponet {
 	
+	@Autowired
+	private IAccountInfoService accountInfoService;
+	
+	/**
+	 * 根据用户编号更新账户信息
+	 * @param amount
+	 */
+	public void updateAccInfo(String userCode, BigDecimal amount){
+		
+		synchronized (AccountComponet.class) {
+			
+			UcAccountInfo ucAccountInfo = accountInfoService.getAccountInfo(userCode);
+			
+			if(ucAccountInfo != null){}
+			
+			
+		}		
+		
+	}
 	
 
 }
