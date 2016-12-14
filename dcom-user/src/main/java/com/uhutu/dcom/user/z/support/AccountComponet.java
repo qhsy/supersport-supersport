d91628f55760dc4a7f087aff942a0eb02ef73f16
upupboys <pangjinhui0522@126.com>
2016-12-14 10:21:00 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/support/AccountComponet.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/support/AccountComponet.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import com.uhutu.dcom.component.z.util.ApplicationSupport;
 import com.uhutu.dcom.user.z.entity.UcAccountInfo;
 import com.uhutu.dcom.user.z.service.IAccountInfoService;
 
@@ -19,17 +19,49 @@ public class AccountComponet {
 	@Autowired
 	private IAccountInfoService accountInfoService;
 	
+	/*实体bean处理处理实例*/
+	private volatile static AccountComponet INSTANCE = null;
+	
+	/**
+	 * 获取实体bean处理实例
+	 * @return AccountComponet
+	 */
+	public static AccountComponet getInstance(){
+		
+		if(INSTANCE == null){
+			
+			synchronized (AccountComponet.class) {
+				
+				if(INSTANCE == null){
+					
+					INSTANCE = (AccountComponet) ApplicationSupport.getBean("accountComponet");
+					
+				}
+				
+			}
+			
+		}
+		
+		return INSTANCE;
+		
+	}
+	
+	
 	/**
 	 * 根据用户编号更新账户信息
 	 * @param amount
 	 */
-	public void updateAccInfo(String userCode, BigDecimal amount){
+	public void updateProfit(String userCode, BigDecimal amount){
 		
 		synchronized (AccountComponet.class) {
 			
 			UcAccountInfo ucAccountInfo = accountInfoService.getAccountInfo(userCode);
 			
-			if(ucAccountInfo != null){}
+			if(ucAccountInfo != null){
+				
+				
+				
+			}
 			
 			
 		}		
