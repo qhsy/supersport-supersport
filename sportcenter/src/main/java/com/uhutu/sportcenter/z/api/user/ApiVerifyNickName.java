faee3a234ada102a45de9f1700867b4495e58824
xiegj <xiegj@ichsy.com>
2016-12-21 19:47:54 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiVerifyNickName.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiVerifyNickName.java
@@ -1,47 +1,39 @@
 package com.uhutu.sportcenter.z.api.user;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.uhutu.dcom.user.z.entity.UcUserinfoExt;
-import com.uhutu.dcom.user.z.service.UserServiceFactory;
 import com.uhutu.sportcenter.z.input.ApiVerifyNickNameInput;
 import com.uhutu.sportcenter.z.result.ApiVerifyNickNameResult;
 import com.uhutu.zoocom.define.DefineUser;
+import com.uhutu.zoocom.helper.MapHelper;
 import com.uhutu.zoocom.root.RootApiBase;
 import com.uhutu.zoocom.z.bean.TopUserFactory;
+import com.uhutu.zoodata.z.helper.JdbcHelper;
 
 /**
  * 昵称校验
+ * 
  * @author 逄小帅
  *
  */
 @Component
 public class ApiVerifyNickName extends RootApiBase<ApiVerifyNickNameInput, ApiVerifyNickNameResult> {
 
-	@Autowired
-	private UserServiceFactory serviceFacotry;
-	
 	@Override
 	protected ApiVerifyNickNameResult process(ApiVerifyNickNameInput input) {
-		
+
 		ApiVerifyNickNameResult result = new ApiVerifyNickNameResult();
-		
-		UcUserinfoExt ucUserinfoExt = serviceFacotry.getUserInfoExtService().queryByNickName(input.getNickName());
-		
-		if(ucUserinfoExt != null){
-			
-			String userCode = TopUserFactory.upUserCallFactory().upUserCodeByAuthToken(input.getZoo().getToken(), DefineUser.Login_System_Default);
-			
-			if(StringUtils.equals(userCode, ucUserinfoExt.getUserCode())){
-				
-				result.inError(81100001);
-				
-			}
-			
+		String userCode = TopUserFactory.upUserCallFactory().upUserCodeByAuthToken(input.getZoo().getToken(),
+				DefineUser.Login_System_Default);
+		UcUserinfoExt ucUserinfoExt = JdbcHelper.queryOne(UcUserinfoExt.class, "", "",
+				"nick_name=:nickName and user_code!=:userCode ",
+				MapHelper.initMap("nickName", input.getNickName(), "userCode", userCode));
+
+		if (ucUserinfoExt != null) {
+			result.inError(81100001);
 		}
-		
+
 		return result;
 	}
 
