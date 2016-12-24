fa24dc67cadf7fadc8c7060be5cc30f4fe6e20de
xiegj <xiegj@ichsy.com>
2016-12-24 21:25:16 +0800

    x

--- dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UserAttenTionPage.java
+++ dcom-user/src/main/java/com/uhutu/dcom/user/z/page/UserAttenTionPage.java
@@ -15,9 +15,7 @@ import com.uhutu.zooweb.define.DefineZooWeb;
  */
 @ZooPage(name = "用户信息", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { DefineZooWeb.PAGE_DEFAULT_OPERATE,
 		DefineWebPage.Page_Query + "=dzoi4699100110171001",
-		DefineWebPage.Page_Grid + "=dzoi41101006,dzoi41101007" }, deploy = { DefineWebDeploy.Url_Query + "=userCode",
-				DefineWebDeploy.Grid_Where
-						+ "=userCode in (select code from uc_userinfo where  mj_flag!='dzsd4699100110010001' )" })
+		DefineWebPage.Page_Grid + "=dzoi41101006,dzoi41101007" }, deploy = { DefineWebDeploy.Url_Query + "=userCode"})
 public class UserAttenTionPage extends RootPageSimple<UcUserinfoExt> {
 
 }
