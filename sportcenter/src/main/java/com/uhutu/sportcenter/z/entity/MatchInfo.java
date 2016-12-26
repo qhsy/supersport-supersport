c1a651ce4cb226dedbb54f1a760beea3b86d3806
upupboys <pangjinhui0522@126.com>
2016-12-26 15:22:23 +0800

    x

---
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/entity/MatchInfo.java
@@ -0,0 +1,89 @@
+package com.uhutu.sportcenter.z.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * 赛事信息
+ * @author 逄小帅
+ *
+ */
+public class MatchInfo {
+	
+	@ApiModelProperty(value="举办者信息")
+	private UserBasicInfo userBasicInfo = new UserBasicInfo();
+	
+	@ApiModelProperty(value="举办地点")
+	private String place;
+	
+	@ApiModelProperty(value="开始时间")
+	private String startTime;
+	
+	@ApiModelProperty(value="结束时间")
+	private String endTime;	
+	
+	@ApiModelProperty(value="封面图")
+	private String cover;
+	
+	@ApiModelProperty(value="赛事标识")
+	private String flag;
+	
+	@ApiModelProperty(value="赛事名称")
+	private String name;
+
+	public UserBasicInfo getUserBasicInfo() {
+		return userBasicInfo;
+	}
+
+	public void setUserBasicInfo(UserBasicInfo userBasicInfo) {
+		this.userBasicInfo = userBasicInfo;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getCover() {
+		return cover;
+	}
+
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+
+	public String getFlag() {
+		return flag;
+	}
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
