de230e2ef1aa21c38a7af2b2f00f12ad78beadf2
upupboys <pangjinhui0522@126.com>
2016-12-28 13:17:52 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/match/ApiShareMatchInfo.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/match/ApiShareMatchInfo.java
@@ -1,10 +1,15 @@
 package com.uhutu.sportcenter.z.api.match;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
-
+import com.uhutu.dcom.content.z.entity.CnMatchInfo;
+import com.uhutu.sportcenter.z.api.util.MatchComponent;
+import com.uhutu.sportcenter.z.entity.MatchInfo;
 import com.uhutu.sportcenter.z.input.ApiShareMatchInfoInput;
 import com.uhutu.sportcenter.z.result.ApiShareMatchInfoResult;
 import com.uhutu.zoocom.root.RootApiBase;
+import com.uhutu.zoodata.z.helper.JdbcHelper;
+import com.uhutu.zooweb.helper.ImageHelper;
 
 /**
  * 赛事详情
@@ -19,7 +24,45 @@ public class ApiShareMatchInfo extends RootApiBase<ApiShareMatchInfoInput, ApiSh
 		
 		ApiShareMatchInfoResult shareMatchInfoResult = new ApiShareMatchInfoResult();
 		
+		CnMatchInfo cnMatchInfo = JdbcHelper.queryOne(CnMatchInfo.class, "code",input.getMatchCode());
+		
+		if(cnMatchInfo != null){
+			
+			MatchInfo matchInfo = initMatchInfo(cnMatchInfo, input.getZoo().getToken(), input.getWidth(), input.getDetailWidth());
+			
+			shareMatchInfoResult.setMatchInfo(matchInfo);
+			
+		}else{
+			
+			shareMatchInfoResult.setError("相关赛事信息不存在");
+			
+			shareMatchInfoResult.setStatus(-1);
+			
+		}
+		
 		return shareMatchInfoResult;
+		
+	}
+	
+	public MatchInfo initMatchInfo(CnMatchInfo cnMatchInfo,String token,int coverWidth,int detaiWidth){
+		
+		MatchInfo matchInfo = new MatchInfo();
+		
+		BeanUtils.copyProperties(cnMatchInfo, matchInfo);
+		
+		String flagName = MatchComponent.getInstance().initFlagName(matchInfo.getFlag());
+		
+		matchInfo.setFlagName(flagName);
+		
+		matchInfo.setUserBasicInfo(MatchComponent.getInstance().initBasicInfo(cnMatchInfo.getUserCode()));
+		
+		String coverSource = ImageHelper.upImageThumbnail(cnMatchInfo.getCover(), coverWidth);
+		
+		matchInfo.setCover(coverSource);
+		
+		matchInfo.setDetails(MatchComponent.getInstance().initDetails(cnMatchInfo.getContent(), detaiWidth));
+		
+		return matchInfo;
 		
 	}
 
--- sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiShareMatchInfoInput.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiShareMatchInfoInput.java
@@ -13,6 +13,12 @@ public class ApiShareMatchInfoInput extends RootApiInput {
 	
 	@ApiModelProperty(value="赛事编号")
 	private String matchCode;
+	
+	@ApiModelProperty(value="封面宽度")
+	private int width;
+	
+	@ApiModelProperty(value="详情宽度")
+	private int detailWidth;
 
 	public String getMatchCode() {
 		return matchCode;
@@ -20,6 +26,22 @@ public class ApiShareMatchInfoInput extends RootApiInput {
 
 	public void setMatchCode(String matchCode) {
 		this.matchCode = matchCode;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getDetailWidth() {
+		return detailWidth;
+	}
+
+	public void setDetailWidth(int detailWidth) {
+		this.detailWidth = detailWidth;
 	}
 
 }
