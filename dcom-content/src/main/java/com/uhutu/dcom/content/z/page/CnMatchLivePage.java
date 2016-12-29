b66d491bcd85af37147e211221e5b1eda4fb96ad
upupboys <pangjinhui0522@126.com>
2016-12-29 17:21:48 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/CnMatchLivePage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/CnMatchLivePage.java
@@ -8,7 +8,7 @@ import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
 @ZooPage(name = "直播信息", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = {
-		DefineWebPage.Page_Add+"=dzoi4699100110010001",
+		DefineWebPage.Page_Add+"=dzoi4699100110010001",DefineWebPage.Page_Edit+"=dzoi4699100110051001",
 		DefineWebPage.Page_Grid+"=dzoi4699100110091001,dzoi4699100110091002" }, deploy = {
 		DefineWebDeploy.Url_Query + "=matchCode", DefineWebDeploy.Grid_Order + "= sort desc,zc desc"})
 public class CnMatchLivePage extends RootPageSimple<CnMatchLive> {
--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/CnMatchVideoPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/CnMatchVideoPage.java
@@ -8,7 +8,7 @@ import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
 @ZooPage(name = "相关视频", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = { 
-		DefineWebPage.Page_Add+"=dzoi4699100110010001",
+		DefineWebPage.Page_Add+"=dzoi4699100110010001",DefineWebPage.Page_Edit+"=dzoi4699100110051001",
 		DefineWebPage.Page_Grid+"=dzoi4699100110091001,dzoi4699100110091002" }, deploy = {
 		DefineWebDeploy.Url_Query + "=matchCode",DefineWebDeploy.Grid_Order + "= sort desc,zc desc"})
 public class CnMatchVideoPage extends RootPageSimple<CnMatchVideo> {
