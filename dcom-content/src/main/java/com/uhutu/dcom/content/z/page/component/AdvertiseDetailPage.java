4ea1fad5b95c572967d57e896968a0223d1eed90
xiegj <xiegj@ichsy.com>
2016-12-29 19:08:30 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/component/AdvertiseDetailPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/component/AdvertiseDetailPage.java
@@ -2,10 +2,12 @@ package com.uhutu.dcom.content.z.page.component;
 
 import com.uhutu.dcom.content.z.entity.CnAdvertiseDetail;
 import com.uhutu.zoocom.baseannotation.ZooPage;
+import com.uhutu.zoocom.define.DefineWebDeploy;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
-@ZooPage(name = "广告图", operates = DefineZooWeb.PAGE_DIALOG_OPERATE)
+@ZooPage(name = "广告图", operates = DefineZooWeb.PAGE_DIALOG_OPERATE, deploy = {
+		DefineWebDeploy.Grid_Where + "=piclink_type not like '%dzsd410710011015%'" })
 public class AdvertiseDetailPage extends RootPageSimple<CnAdvertiseDetail> {
 
 }
