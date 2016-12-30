6df88e414c33b5f4dffbeab70768cb1241e6c4f3
upupboys <pangjinhui0522@126.com>
2016-12-30 13:36:01 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/entity/MatchLiveInfo.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/entity/MatchLiveInfo.java
@@ -1,5 +1,11 @@
 package com.uhutu.sportcenter.z.entity;
 
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.uhutu.zoocom.helper.DateHelper;
+
 import io.swagger.annotations.ApiModelProperty;
 
 /**
@@ -47,6 +53,19 @@ public class MatchLiveInfo {
 	}
 
 	public String getStartTime() {
+		
+		if(StringUtils.isNotEmpty(startTime)){
+			
+			Date startDate = DateHelper.parseDate(startTime);
+			
+			if(startDate != null){
+				
+				startTime = DateHelper.upDate(startDate,"yyyy-MM-dd HH:mm");
+				
+			}
+			
+		}
+		
 		return startTime;
 	}
 
