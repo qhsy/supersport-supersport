edb4766dcba87f7049e4d40d550b60e79be6a29f
upupboys <pangjinhui0522@126.com>
2017-01-06 16:47:15 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiAttendFlag.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/user/ApiAttendFlag.java
@@ -1,8 +1,10 @@
 package com.uhutu.sportcenter.z.api.user;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.uhutu.dcom.config.enums.SystemEnum;
+import com.uhutu.dcom.content.z.entity.CnLiveVideoInfo;
 import com.uhutu.dcom.user.z.entity.UcAttentionInfo;
 import com.uhutu.sportcenter.z.input.ApiAttendFlagInput;
 import com.uhutu.sportcenter.z.result.ApiAttendFlagResult;
@@ -17,16 +19,51 @@ public class ApiAttendFlag extends RootApiToken<ApiAttendFlagInput, ApiAttendFla
 
 		ApiAttendFlagResult attendFlagResult = new ApiAttendFlagResult();
 
-		UcAttentionInfo attentionInfo = JdbcHelper.queryOne(UcAttentionInfo.class, "attention", upUserCode(),
-				"beAttention", input.getBeAttend(), "status", SystemEnum.NORMAL.getCode());
-		
-		if(attentionInfo != null){
-			
-			attendFlagResult.setAttendFlag(true);
-			
+		switch (input.getOperFlag()) {
+		case "live":
+			attendFlagResult.setAttendFlag(initLiveFlag(upUserCode(), input.getRoomId()));
+			break;
+
+		default:
+			attendFlagResult.setAttendFlag(initFlag(upUserCode(), input.getBeAttend()));
+			break;
 		}
 
 		return attendFlagResult;
 	}
+	
+	public boolean initFlag(String attention,String beAttention){
+		
+		boolean flag = false;
+		
+		UcAttentionInfo attentionInfo = JdbcHelper.queryOne(UcAttentionInfo.class, "attention", attention,
+				"beAttention", beAttention, "status", SystemEnum.NORMAL.getCode());
+		
+		if(attentionInfo != null){
+			
+			flag = true;
+			
+		}
+		
+		return flag;
+		
+	}
+	
+	public boolean initLiveFlag(String attention,String roomId){
+		
+		boolean flag = false;
+		
+		CnLiveVideoInfo cnVideoInfo = JdbcHelper.queryOne(CnLiveVideoInfo.class, "code",roomId);
+		
+		if(cnVideoInfo != null && StringUtils.isNotEmpty(cnVideoInfo.getUserCode())){
+			
+			flag = initFlag(attention, cnVideoInfo.getUserCode());
+			
+		}
+		
+		return flag;
+		
+	}
+	
 
 }
--- sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiAttendFlagInput.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiAttendFlagInput.java
@@ -13,6 +13,12 @@ public class ApiAttendFlagInput extends RootApiInput {
 	
 	@ApiModelProperty(value="被关注的用户编号")
 	private String beAttend;
+	
+	@ApiModelProperty(value="直播房间号")
+	private String roomId;
+	
+	@ApiModelProperty(value="操作标识")
+	private String operFlag;
 
 	public String getBeAttend() {
 		return beAttend;
@@ -20,6 +26,22 @@ public class ApiAttendFlagInput extends RootApiInput {
 
 	public void setBeAttend(String beAttend) {
 		this.beAttend = beAttend;
+	}
+
+	public String getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(String roomId) {
+		this.roomId = roomId;
+	}
+
+	public String getOperFlag() {
+		return operFlag;
+	}
+
+	public void setOperFlag(String operFlag) {
+		this.operFlag = operFlag;
 	}
 
 }
