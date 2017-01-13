f6449154fdc0be26fe78c877e26cebf5a709dc6f
upupboys <pangjinhui0522@126.com>
2017-01-13 10:12:56 +0800

    x

--- dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnMatchLink.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/entity/CnMatchLink.java
@@ -22,25 +22,25 @@ import com.uhutu.zoodata.dbbase.BaseEntity;
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = { "matchCode" }))
 public class CnMatchLink extends BaseEntity {
 	
-	@ZooData(name = "赛事编号",  inc = DefineWebInc.Url_Param + "=matchCode",sort = {DefineWebPage.Page_Query + "=1", DefineWebPage.Page_Add + "=" + DefineWebSort.Sort_Process})
+	@ZooData(name = "赛事编号",  inc = DefineWebInc.Url_Param + "=matchCode",sort = {DefineWebPage.Page_Query + "=1",DefineWebPage.Page_Edit+"="+DefineWebSort.Sort_Process, DefineWebPage.Page_Add + "=" + DefineWebSort.Sort_Process})
 	@Column(length=50)
 	private String matchCode;
 	
-	@ZooData(name = "标题", require = "1",verify = { DefineWebVerify.Max_Length + "=20" })
+	@ZooData(name = "标题", require = "1",verify = { DefineWebVerify.Max_Length + "=20" },sort = {DefineWebPage.Page_Query + "=1"})
 	@Column(length=50)
 	private String title;
 	
-	@ZooData(name = "跳转类型", require = "1",element = DefineWebElement.Select,inc = {DefineWebInc.System_Define + "=dzsd410710011005" })
+	@ZooData(name = "跳转类型", require = "1",element = DefineWebElement.Select,inc = {DefineWebInc.System_Define + "=dzsd410710011005" },sort = {DefineWebPage.Page_Query + "=1"})
 	@Column(length=50)
 	private String linkType;
 	
-	@ZooData(name = "跳转内容", require = "1")
+	@ZooData(name = "跳转内容", require = "1",sort = {DefineWebPage.Page_Query + "=1"})
 	private String linkContent;
 	
-	@ZooData(name = "跳转后的标题")
+	@ZooData(name = "跳转后的标题",sort = {DefineWebPage.Page_Query + "=1"})
 	private String linkTitle;
 	
-	@ZooData(name = "状态",element = DefineWebElement.Select,inc = {DefineWebInc.System_Define + "=dzsd410710011013" })
+	@ZooData(name = "状态",element = DefineWebElement.Select,inc = {DefineWebInc.System_Define + "=dzsd410710011013" },sort = {DefineWebPage.Page_Query + "=1"})
 	private String status;
 
 	public String getMatchCode() {
--- dcom-content/src/main/java/com/uhutu/dcom/content/z/page/CnMatchLinkPage.java
+++ dcom-content/src/main/java/com/uhutu/dcom/content/z/page/CnMatchLinkPage.java
@@ -3,10 +3,11 @@ package com.uhutu.dcom.content.z.page;
 import com.uhutu.dcom.content.z.entity.CnMatchLink;
 import com.uhutu.zoocom.baseannotation.ZooPage;
 import com.uhutu.zoocom.define.DefineWebDeploy;
+import com.uhutu.zoocom.define.DefineWebPage;
 import com.uhutu.zoocom.root.RootPageSimple;
 import com.uhutu.zooweb.define.DefineZooWeb;
 
-@ZooPage(name = "赛事跳转", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = {DefineZooWeb.PAGE_DEFAULT_OPERATE}, 
+@ZooPage(name = "赛事跳转", pages = { DefineZooWeb.PAGE_DEFAULT_SUB }, operates = {DefineWebPage.Page_Edit+"=dzoi4699100110051001",DefineWebPage.Page_Grid+"=dzoi4699100110091001,dzoi4699100110091002"}, 
 deploy = {DefineWebDeploy.Url_Query + "=matchCode"})
 public class CnMatchLinkPage extends RootPageSimple<CnMatchLink> {
 
