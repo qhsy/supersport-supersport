bac70445a8608bc97afad81bc3ba092c67ba3ad2
upupboys <pangjinhui0522@126.com>
2017-02-09 17:09:27 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiOperContent.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/content/ApiOperContent.java
@@ -1,13 +1,18 @@
 package com.uhutu.sportcenter.z.api.content;
 
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.uhutu.dcom.config.enums.SystemEnum;
+import com.uhutu.dcom.content.z.entity.CnContentBasicinfo;
 import com.uhutu.dcom.content.z.enums.ContentEnum;
 import com.uhutu.dcom.content.z.service.ContentServiceFactory;
 import com.uhutu.sportcenter.z.input.ApiOperContentInput;
 import com.uhutu.sportcenter.z.result.ApiOperContentResult;
 import com.uhutu.zoocom.root.RootApiToken;
+import com.uhutu.zoodata.z.helper.JdbcHelper;
 
 /**
  * 
@@ -31,6 +36,11 @@ public class ApiOperContent extends RootApiToken<ApiOperContentInput, ApiOperCon
 			delete(input.getContentCode());
 			
 			break;
+		case "redpack":
+			
+			operRedPack(input.getContentCode());
+			
+			break;
 
 		default:
 			break;
@@ -46,6 +56,32 @@ public class ApiOperContent extends RootApiToken<ApiOperContentInput, ApiOperCon
 	public int delete(String contentCode){
 		
 		return serviceFactory.getContentBasicinfoService().updateStatus(contentCode, ContentEnum.invalid.getCode());
+		
+	}
+	
+	/**
+	 * 打赏开关
+	 * @param contentCode
+	 */
+	public void operRedPack(String contentCode){
+		
+		if(StringUtils.isNotEmpty(contentCode)){
+			
+			CnContentBasicinfo basicinfo = JdbcHelper.queryOne(CnContentBasicinfo.class, "code", contentCode);
+			
+			if(basicinfo != null){
+				
+				String redPackFlag = basicinfo.getRedPackFlag();
+					
+				redPackFlag = StringUtils.equals(SystemEnum.YES.getCode(), redPackFlag) ? SystemEnum.NO.getCode() : SystemEnum.YES.getCode();
+					
+				basicinfo.setRedPackFlag(redPackFlag);
+				
+				JdbcHelper.update(basicinfo, "redPackFlag", "code");
+				
+			}
+			
+		}
 		
 	}
 
