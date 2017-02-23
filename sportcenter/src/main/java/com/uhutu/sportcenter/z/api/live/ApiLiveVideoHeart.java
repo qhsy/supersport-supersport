96b6e7c3b2ea46aaa9bb7c6be1469233fac4d115
upupboys <pangjinhui0522@126.com>
2017-02-23 10:15:47 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/api/live/ApiLiveVideoHeart.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/api/live/ApiLiveVideoHeart.java
@@ -29,7 +29,6 @@ public class ApiLiveVideoHeart extends RootApiToken<ApiLiveVideoHeartInput, ApiL
 				"user_code=:user_code", MapHelper.initMap("user_code", upUserCode()));
 		if (detail != null) {
 			detail.setChannelId(input.getChannelId());
-			detail.setStreamUrl(input.getStreamUrl());
 			detail.setLength(input.getLength());
 			detail.setWatch(input.getWatch());
 			detail.setPraise(input.getPraise());
