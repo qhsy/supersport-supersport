5d338c167f57a0db0d2f0150012cb936028f8f91
xiegj <xiegj@ichsy.com>
2017-06-09 18:12:41 +0800

    x

--- sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiSavePointsInput.java
+++ sportcenter/src/main/java/com/uhutu/sportcenter/z/input/ApiSavePointsInput.java
@@ -6,7 +6,7 @@ import io.swagger.annotations.ApiModelProperty;
 
 public class ApiSavePointsInput extends RootApiInput {
 
-	@ApiModelProperty(name = "内容编号", required = true, value = "编号", example = "v1.1.0")
+	@ApiModelProperty(name = "内容编号", required = true, value = "编号", example = "v1.1.0,启动startApp,注册：register")
 	private String code;
 
 	@ApiModelProperty(name = "积分类型", required = true, value = "积分类型", example = "积分类型:JFLXBH170519110001~07,特效类型:TXLXBH0001~03")
